f03146cc56944aa45ed273e2dd6eb9d7c39778fb
Jitendra Choudhary <jitendrachoudhary@Jitendras-MacBook-Pro.local>
2022-09-28 16:08:55 +0530

    [hlm-707]: [hlm-707]: Skeleton for sync service + clean up - Add endpoint to accept sync request with request info - jitendrac-egov|jayantp-egov

--- digit-health-services/health-sync-service/src/main/java/org/digit/health/sync/web/controller/SyncController.java
+++ digit-health-services/health-sync-service/src/main/java/org/digit/health/sync/web/controller/SyncController.java
@@ -1,18 +1,24 @@
 package org.digit.health.sync.web.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.digit.health.sync.web.models.SyncRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @Slf4j
-@RequestMapping("/sync/v1")
+@RequestMapping("/sync/v1/file")
 public class SyncController {
 
-    @GetMapping("/up")
-    public ResponseEntity<String> syncUp() {
+    @PostMapping("/_create")
+    public ResponseEntity<String> syncUp(@RequestBody @Valid SyncRequest syncRequest) {
+        log.info("Logged Sync Request",syncRequest.getRequestInfo().toString());
         return ResponseEntity.ok("Sync request submitted");
     }
 }
