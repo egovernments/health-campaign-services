1e69c894b129b18f5a11e9299ac7d09a2483dda3
Shiva <shvburade@gmail.com>
2022-09-30 16:13:55 +0530

    [hlm-833]: Added required spring annotations

--- digit-health-services/health-sync-service/src/main/java/org/digit/health/sync/service/CompressionService.java
+++ digit-health-services/health-sync-service/src/main/java/org/digit/health/sync/service/CompressionService.java
@@ -2,13 +2,17 @@ package org.digit.health.sync.service;
 
 
 import org.digit.health.sync.service.compressor.Compressor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 
+@Service
 public class CompressionService {
-    Compressor compressor;
+    @Autowired
+    private final Compressor compressor;
 
     public CompressionService(Compressor compressor){
         this.compressor = compressor;
--- digit-health-services/health-sync-service/src/main/java/org/digit/health/sync/service/compressor/GzipCompressor.java
+++ digit-health-services/health-sync-service/src/main/java/org/digit/health/sync/service/compressor/GzipCompressor.java
@@ -1,9 +1,13 @@
 package org.digit.health.sync.service.compressor;
 
+import org.springframework.stereotype.Component;
+
 import java.io.*;
 import java.util.zip.GZIPInputStream;
 import java.util.zip.ZipException;
 
+
+@Component
 public class GzipCompressor implements Compressor{
     @Override
     public BufferedReader decompress(InputStream stream) throws IOException {
