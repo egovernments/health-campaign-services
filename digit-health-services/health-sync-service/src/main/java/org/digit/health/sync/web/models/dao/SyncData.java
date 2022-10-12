b7ed5b5e4f82cf423bd4b63959e56286920b49cd
Jitendra Choudhary <jitendrachoudhary@Jitendras-MacBook-Pro.local>
2022-10-12 12:35:24 +0530

    [hlm-707]:  Fixed failing local build - jitendra

--- digit-health-services/health-sync-service/src/main/java/org/digit/health/sync/web/models/dao/SyncData.java
+++ digit-health-services/health-sync-service/src/main/java/org/digit/health/sync/web/models/dao/SyncData.java
@@ -6,10 +6,6 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
 
 @Data
 @NoArgsConstructor
