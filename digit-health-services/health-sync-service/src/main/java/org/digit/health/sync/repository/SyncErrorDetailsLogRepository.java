1d5b3dae9156beaae965655a72a7ded426068abe
jayant.porwal <jayant.porwal@egovernments.org>
2022-10-14 10:15:20 +0530

    [hlm-707]: Added search for SyncErrorDetailsLog - Some minor refactoring - JUnits

--- digit-health-services/health-sync-service/src/main/java/org/digit/health/sync/repository/SyncErrorDetailsLogRepository.java
+++ digit-health-services/health-sync-service/src/main/java/org/digit/health/sync/repository/SyncErrorDetailsLogRepository.java
@@ -2,6 +2,10 @@ package org.digit.health.sync.repository;
 
 import org.digit.health.sync.web.models.dao.SyncErrorDetailsLogData;
 
+import java.util.List;
+
 public interface SyncErrorDetailsLogRepository {
     SyncErrorDetailsLogData save(SyncErrorDetailsLogData data);
+
+    List<SyncErrorDetailsLogData> find(SyncErrorDetailsLogData data);
 }
--- digit-health-services/health-sync-service/src/test/java/org/digit/health/sync/repository/DefaultSyncErrorDetailsLogRepositoryTest.java
+++ digit-health-services/health-sync-service/src/test/java/org/digit/health/sync/repository/DefaultSyncErrorDetailsLogRepositoryTest.java
@@ -4,18 +4,28 @@ import org.digit.health.sync.kafka.Producer;
 import org.digit.health.sync.repository.enums.RepositoryErrorCode;
 import org.digit.health.sync.web.models.dao.SyncErrorDetailsLogData;
 import org.egov.tracer.model.CustomException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class DefaultSyncErrorDetailsLogRepositoryTest {
@@ -23,10 +33,21 @@ class DefaultSyncErrorDetailsLogRepositoryTest {
     @Mock
     private Producer producer;
 
+    @Mock
+    private NamedParameterJdbcTemplate jdbcTemplate;
+
+    private SyncErrorDetailsLogRepository repository;
+
+    @BeforeEach
+    void setUp() {
+        SyncErrorDetailsLogQueryBuilder queryBuilder = new DefaultSyncErrorDetailsLogQueryBuilder();
+        repository =
+                new DefaultSyncErrorDetailsLogRepository(producer, jdbcTemplate, queryBuilder);
+    }
+
     @Test
     @DisplayName("should save sync error details log and return the same successfully")
     void shouldSaveSyncErrorDetailsLogDataAndReturnSameSuccessfully() {
-        SyncErrorDetailsLogRepository repository = new DefaultSyncErrorDetailsLogRepository(producer);
         SyncErrorDetailsLogData expectedData = SyncErrorDetailsLogData.builder().build();
 
         SyncErrorDetailsLogData actualData = repository.save(expectedData);
@@ -39,8 +60,6 @@ class DefaultSyncErrorDetailsLogRepositoryTest {
     @Test
     @DisplayName("should return null in case the payload is null")
     void shouldReturnNullInCaseThePayloadIsNull() {
-        SyncErrorDetailsLogRepository repository = new DefaultSyncErrorDetailsLogRepository(producer);
-
         SyncErrorDetailsLogData actualData = repository.save(null);
 
         assertNull(actualData);
@@ -51,7 +70,6 @@ class DefaultSyncErrorDetailsLogRepositoryTest {
     @Test
     @DisplayName("should throw custom exception with proper error code in case of any error")
     void shouldThrowCustomExceptionWithErrorCodeInCaseOfAnyError() {
-        SyncErrorDetailsLogRepository repository = new DefaultSyncErrorDetailsLogRepository(producer);
         SyncErrorDetailsLogData expectedData = SyncErrorDetailsLogData.builder().build();
         doThrow(new RuntimeException("some_message")).when(producer)
                 .send(DefaultSyncErrorDetailsLogRepository.SAVE_KAFKA_TOPIC, expectedData);
@@ -67,5 +85,90 @@ class DefaultSyncErrorDetailsLogRepositoryTest {
         assertEquals(RepositoryErrorCode.SAVE_ERROR.name(), ex.getCode());
         assertEquals(RepositoryErrorCode.SAVE_ERROR.message("some_message"),
                 ex.getMessage());
+    }
+
+    @Test
+    @DisplayName("should return a list of sync error details log based on syncId")
+    void shouldReturnAListOfSyncErrorDetailsLogBasedOnSyncId() {
+        SyncErrorDetailsLogData syncErrorDetailsLogData = SyncErrorDetailsLogData.builder()
+                .tenantId("some-tenant-id")
+                .syncId("some-sync-id")
+                .build();
+        List<SyncErrorDetailsLogData> expectedDataList = Collections
+                .singletonList(syncErrorDetailsLogData);
+        when(jdbcTemplate.query(
+                        eq("SELECT * FROM sync_error_details_log WHERE tenantId=:tenantId AND syncId=:syncId"),
+                        any(Map.class),
+                        any(BeanPropertyRowMapper.class))).thenReturn(expectedDataList);
+
+        List<SyncErrorDetailsLogData> actualDataList = repository
+                .find(syncErrorDetailsLogData);
+
+        assertEquals(expectedDataList, actualDataList);
+    }
+
+    @Test
+    @DisplayName("should return a list of sync error details log based on recordIdType")
+    void shouldReturnAListOfSyncErrorDetailsLogBasedOnRecordIdType() {
+        SyncErrorDetailsLogData syncErrorDetailsLogData = SyncErrorDetailsLogData.builder()
+                .tenantId("some-tenant-id")
+                .recordIdType("REGISTRATION")
+                .build();
+        List<SyncErrorDetailsLogData> expectedDataList = Collections
+                .singletonList(syncErrorDetailsLogData);
+        when(jdbcTemplate.query(
+                eq("SELECT * FROM sync_error_details_log WHERE tenantId=:tenantId " +
+                        "AND recordIdType=:recordIdType"),
+                any(Map.class),
+                any(BeanPropertyRowMapper.class))).thenReturn(expectedDataList);
+
+        List<SyncErrorDetailsLogData> actualDataList = repository
+                .find(syncErrorDetailsLogData);
+
+        assertEquals(expectedDataList, actualDataList);
+    }
+
+    @Test
+    @DisplayName("should return a list of sync error details log based on recordId")
+    void shouldReturnAListOfSyncErrorDetailsLogBasedOnRecordId() {
+        SyncErrorDetailsLogData syncErrorDetailsLogData = SyncErrorDetailsLogData.builder()
+                .tenantId("some-tenant-id")
+                .recordId("some-id")
+                .build();
+        List<SyncErrorDetailsLogData> expectedDataList = Collections
+                .singletonList(syncErrorDetailsLogData);
+        when(jdbcTemplate.query(
+                eq("SELECT * FROM sync_error_details_log WHERE tenantId=:tenantId " +
+                        "AND recordId=:recordId"),
+                any(Map.class),
+                any(BeanPropertyRowMapper.class))).thenReturn(expectedDataList);
+
+        List<SyncErrorDetailsLogData> actualDataList = repository
+                .find(syncErrorDetailsLogData);
+
+        assertEquals(expectedDataList, actualDataList);
+    }
+
+    @Test
+    @DisplayName("should return a list of sync error details log based on syncId, recordId and recordIdType")
+    void shouldReturnAListOfSyncErrorDetailsLogBasedOnSyncIdRecordIdAndRecordIdType() {
+        SyncErrorDetailsLogData syncErrorDetailsLogData = SyncErrorDetailsLogData.builder()
+                .tenantId("some-tenant-id")
+                .syncId("some-sync-id")
+                .recordId("some-id")
+                .recordIdType("REGISTRATION")
+                .build();
+        List<SyncErrorDetailsLogData> expectedDataList = Collections
+                .singletonList(syncErrorDetailsLogData);
+        when(jdbcTemplate.query(
+                eq("SELECT * FROM sync_error_details_log WHERE tenantId=:tenantId " +
+                        "AND syncId=:syncId AND recordId=:recordId AND recordIdType=:recordIdType"),
+                any(Map.class),
+                any(BeanPropertyRowMapper.class))).thenReturn(expectedDataList);
+
+        List<SyncErrorDetailsLogData> actualDataList = repository
+                .find(syncErrorDetailsLogData);
+
+        assertEquals(expectedDataList, actualDataList);
     }
 }
