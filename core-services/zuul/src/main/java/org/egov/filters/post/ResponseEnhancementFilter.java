3ed4a4f65b5d766b24e00bf18357059b403ef133
jitendrachoudhary <jitendra.choudhary@egovernments.org>
2022-11-22 13:55:20 +0530

    [hlm-1096]: Added compression

--- core-services/zuul/src/main/java/org/egov/filters/post/ResponseEnhancementFilter.java
+++ core-services/zuul/src/main/java/org/egov/filters/post/ResponseEnhancementFilter.java
@@ -1,22 +1,42 @@
 package org.egov.filters.post;
 
+import com.google.common.io.CharStreams;
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.zip.GZIPOutputStream;
 
 import static org.egov.constants.RequestContextConstants.CORRELATION_ID_KEY;
 
 /**
  * Sets the correlation id to the response header.
  */
+@Slf4j
 @Component
 public class ResponseEnhancementFilter extends ZuulFilter {
 
-    private static final String CORRELATION_HEADER_NAME = "x-correlation-id";
+    private static final String CORRELATION_HEADER_NAME = "y-correlation-id";
     private static final String RECEIVED_RESPONSE_MESSAGE = "Received response code: {} from upstream URI {}";
     private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    public static String compress(String str) throws IOException {
+        if (str == null || str.length() == 0) {
+            return str;
+        }
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        GZIPOutputStream gzip = new GZIPOutputStream(out);
+        gzip.write(str.getBytes());
+        gzip.close();
+        return out.toString("UTF-8");
+    }
 
     @Override
     public String filterType() {
@@ -38,7 +58,12 @@ public class ResponseEnhancementFilter extends ZuulFilter {
         RequestContext ctx = RequestContext.getCurrentContext();
         ctx.addZuulResponseHeader(CORRELATION_HEADER_NAME, getCorrelationId());
         ctx.addZuulResponseHeader("Cache-Control", "no-cache, no-store, max-age=0, must-revalidate");
-
+        String resBody = readResponseBody(ctx);
+        try {
+            compress(resBody);
+        } catch (Exception err) {
+            log.error(err.toString());
+        }
         return null;
     }
 
@@ -47,5 +72,19 @@ public class ResponseEnhancementFilter extends ZuulFilter {
         logger.info(RECEIVED_RESPONSE_MESSAGE,
             ctx.getResponse().getStatus(), ctx.getRequest().getRequestURI());
         return (String) ctx.get(CORRELATION_ID_KEY);
+    }
+
+    private String readResponseBody(RequestContext ctx) {
+        String responseBody = null;
+        try (final InputStream responseDataStream = ctx.getResponseDataStream()) {
+            responseBody = CharStreams.toString(new InputStreamReader(responseDataStream, "UTF-8"));
+            //ctx.setResponseBody(responseBody);
+        } catch (IOException e) {
+            log.error("Error reading body", e);
+        } catch (Exception e) {
+            log.error(e.toString());
+            log.error("Exception while reading response body: " + e.getMessage());
+        }
+        return responseBody;
     }
 }
