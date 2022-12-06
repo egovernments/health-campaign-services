c6679545a210da24ae75bf2f810599e4139c8935
jayant.porwal <jayant.porwal@egovernments.org>
2022-12-06 15:23:04 +0530

    [hlm-699]: Generating Id using IdGenService

--- health-services/product/src/main/java/org/egov/product/service/ProductVariantService.java
+++ health-services/product/src/main/java/org/egov/product/service/ProductVariantService.java
@@ -1,15 +1,37 @@
 package org.egov.product.service;
 
+import org.egov.product.util.IdGenService;
 import org.egov.product.web.models.ProductVariant;
 import org.egov.product.web.models.ProductVariantRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.IntStream;
 
 @Service
 public class ProductVariantService {
 
+    private final IdGenService idGenService;
+
+    @Autowired
+    public ProductVariantService(IdGenService idGenService) {
+
+        this.idGenService = idGenService;
+    }
+
     public List<ProductVariant> create(ProductVariantRequest request) {
-        return null;
+        Optional<ProductVariant> anyProductVariant = request.getProductVariant()
+                .stream().findAny();
+        String tenantId = null;
+        if (anyProductVariant.isPresent()) {
+            tenantId = anyProductVariant.get().getTenantId();
+        }
+        List<String> idList = idGenService.getIdList(request.getRequestInfo(), tenantId,
+                "productVariant", "", request.getProductVariant().size());
+        IntStream.range(0, request.getProductVariant().size())
+                .forEach(i -> request.getProductVariant().get(i).setId(idList.get(i)));
+        return request.getProductVariant();
     }
 }
---
+++ health-services/product/src/test/java/org/egov/product/service/ProductVariantServiceTest.java
@@ -0,0 +1,56 @@
+package org.egov.product.service;
+
+import org.egov.common.contract.request.RequestInfo;
+import org.egov.product.helper.ProductVariantRequestTestBuilder;
+import org.egov.product.util.IdGenService;
+import org.egov.product.web.models.ProductVariant;
+import org.egov.product.web.models.ProductVariantRequest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class ProductVariantServiceTest {
+
+    @InjectMocks
+    private ProductVariantService productVariantService;
+
+    @Mock
+    private IdGenService idGenService;
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    @DisplayName("should enrich the formatted id in product variants")
+    void shouldEnrichTheFormattedIdInProductVariants() {
+        ProductVariantRequest request = ProductVariantRequestTestBuilder.builder()
+                .withOneProductVariantAndApiOperationNull()
+                .build();
+        List<String> idList = new ArrayList<>();
+        idList.add("some-id");
+        when(idGenService.getIdList(any(RequestInfo.class),
+                any(String.class),
+                any(String.class), eq(""), anyInt()))
+                .thenReturn(idList);
+
+        List<ProductVariant> productVariants = productVariantService.create(request);
+
+        assertEquals("some-id", productVariants.get(0).getId());
+    }
+}
