a39b439b3d3739639d21d9780606dca22fd839f2
jitendrachoudhary <jitendra.choudhary@egovernments.org>
2022-12-08 15:54:25 +0530

    [hlm-700]: Added tables

--- health-services/project/src/main/resources/db/migration/main/V20221202180100__project_create_ddl.sql
+++ health-services/project/src/main/resources/db/migration/main/V20221202180100__project_create_ddl.sql
@@ -1,1 +1,17 @@
--- SQL to migrate goes here
+CREATE TABLE project (
+                         id varchar(50) NOT NULL PRIMARY KEY,
+                         tenantId varchar(255) NOT NULL,
+                         projectTypeId varchar(255),
+                         addressId varchar(255),
+                         startDate BIGINT,
+                         endDate BIGINT,
+                         isTaskEnabled BOOLEAN,
+                         parent varchar(255),
+                         additionalDetails json,
+                         createdBy varchar(255),
+                         createdTime BIGINT,
+                         lastModifiedBy varchar(255),
+                         lastModifiedTime BIGINT,
+                         rowVersion BIGINT,
+                         isDeleted BOOLEAN
+);
