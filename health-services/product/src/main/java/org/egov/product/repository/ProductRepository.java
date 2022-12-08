e6207b6c59d54711d70c666235f399fe84ed6c63
jayant.porwal <jayant.porwal@egovernments.org>
2022-12-08 10:20:01 +0530

    [hlm-699]: Removed unnecessary exception

--- health-services/product/src/main/java/org/egov/product/repository/ProductRepository.java
+++ health-services/product/src/main/java/org/egov/product/repository/ProductRepository.java
@@ -2,12 +2,10 @@ package org.egov.product.repository;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -28,17 +26,13 @@ public class ProductRepository {
         String query = String.format("SELECT id FROM product WHERE id IN (:productIds) fetch first %s rows only", productIds.size());
         Map<String, Object> paramMap = new HashMap<>();
         paramMap.put("productIds", productIds);
-        try {
-            return namedParameterJdbcTemplate.queryForObject(query, paramMap, (resultSet, rowNumber) -> {
-                List<String> validProductIds = new ArrayList<>();
+        return namedParameterJdbcTemplate.queryForObject(query, paramMap, (resultSet, rowNumber) -> {
+            List<String> validProductIds = new ArrayList<>();
+            validProductIds.add(resultSet.getString(1));
+            while (resultSet.next()) {
                 validProductIds.add(resultSet.getString(1));
-                while (resultSet.next()) {
-                    validProductIds.add(resultSet.getString(1));
-                }
-                return validProductIds;
-            });
-        } catch (EmptyResultDataAccessException e) {
-            return Collections.emptyList();
-        }
+            }
+            return validProductIds;
+        });
     }
 }
