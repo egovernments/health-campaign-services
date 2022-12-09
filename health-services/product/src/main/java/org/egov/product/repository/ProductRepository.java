5c7d7bba2153a4e387b76b85716196c701fd1c45
jayant.porwal <jayant.porwal@egovernments.org>
2022-12-09 17:58:10 +0530

    [hlm-699]: Return empty list in case no rows found

--- health-services/product/src/main/java/org/egov/product/repository/ProductRepository.java
+++ health-services/product/src/main/java/org/egov/product/repository/ProductRepository.java
@@ -3,10 +3,12 @@ package org.egov.product.repository;
 import lombok.extern.slf4j.Slf4j;
 import org.egov.common.producer.Producer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -30,13 +32,17 @@ public class ProductRepository {
         String query = String.format("SELECT id FROM product WHERE id IN (:productIds) AND isDeleted = false fetch first %s rows only", productIds.size());
         Map<String, Object> paramMap = new HashMap<>();
         paramMap.put("productIds", productIds);
-        return namedParameterJdbcTemplate.queryForObject(query, paramMap, (resultSet, rowNumber) -> {
-            List<String> validProductIds = new ArrayList<>();
-            validProductIds.add(resultSet.getString(1));
-            while (resultSet.next()) {
+        try {
+            return namedParameterJdbcTemplate.queryForObject(query, paramMap, (resultSet, rowNumber) -> {
+                List<String> validProductIds = new ArrayList<>();
                 validProductIds.add(resultSet.getString(1));
-            }
-            return validProductIds;
-        });
+                while (resultSet.next()) {
+                    validProductIds.add(resultSet.getString(1));
+                }
+                return validProductIds;
+            });
+        } catch (EmptyResultDataAccessException e) {
+            return Collections.emptyList();
+        }
     }
 }
