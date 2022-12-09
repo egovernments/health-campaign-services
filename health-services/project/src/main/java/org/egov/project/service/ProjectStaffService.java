c11c94aa0e7d6affbc253efd38f449fe44612560
jitendrachoudhary <jitendra.choudhary@egovernments.org>
2022-12-09 10:58:46 +0530

    [hlm-700]: Added Project Repository and added test cases for the project exists

--- health-services/project/src/main/java/org/egov/project/service/ProjectStaffService.java
+++ health-services/project/src/main/java/org/egov/project/service/ProjectStaffService.java
@@ -28,6 +28,41 @@ public class ProjectStaffService {
     }
 
     ProjectStaff create(ProjectStaff projectStaff){
-        return projectStaffRepository.save(projectStaff);
+        
+        // TODO  - Check if project staff data is valid or not
+        validateProjectStaff(projectStaff);
+
+        // TODO - Check if project staff exists in redis or db
+
+        checkIfExists(projectStaff);
+
+        // TODO - Check if this user is present in the user service
+
+        checkIfUserIsValid(projectStaff);
+
+        // TODO - CHeck if this project exists or not
+        checkIfProjectExists(projectStaff);
+
+        // TODO - Generate Project Staff Id using id gen
+        generateProjectStaffId(projectStaff);
+
+        ProjectStaff savedStaff = projectStaffRepository.save(projectStaff);
+
+        return savedStaff;
+    }
+
+    private void generateProjectStaffId(ProjectStaff projectStaff) {
+    }
+
+    private void checkIfProjectExists(ProjectStaff projectStaff) {
+    }
+
+    private void checkIfUserIsValid(ProjectStaff projectStaff) {
+    }
+
+    private void checkIfExists(ProjectStaff projectStaff) {
+    }
+
+    private void validateProjectStaff(ProjectStaff projectStaff) {
     }
 }
