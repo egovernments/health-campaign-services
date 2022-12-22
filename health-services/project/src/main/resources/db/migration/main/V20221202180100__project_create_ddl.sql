205ba8da59f0b41ebef5437f96a463519c3c796a
jitendrachoudhary <jitendra.choudhary@egovernments.org>
2022-12-22 15:46:21 +0530

    Updated the fly way migration files

--- health-services/project/src/main/resources/db/migration/main/V20221202180100__project_create_ddl.sql
+++ health-services/project/src/main/resources/db/migration/main/V20221202180100__project_create_ddl.sql
@@ -1,18 +1,20 @@
-CREATE TABLE project (
-                         id varchar(50) NOT NULL PRIMARY KEY,
-                         tenantId varchar(255) NOT NULL,
-                         projectTypeId varchar(255),
-                         addressId varchar(255),
-                         startDate BIGINT,
-                         endDate BIGINT,
-                         isTaskEnabled BOOLEAN,
-                         parent varchar(255),
-                         projectHierarchy varchar(255),
-                         additionalDetails json,
-                         createdBy varchar(255),
-                         createdTime BIGINT,
-                         lastModifiedBy varchar(255),
-                         lastModifiedTime BIGINT,
-                         rowVersion BIGINT,
-                         isDeleted BOOLEAN
+CREATE TABLE project
+(
+    id                character varying(64),
+    tenantId          character varying(1000),
+    projectTypeId     character varying(64),
+    addressId         character varying(64),
+    startDate         bigint,
+    endDate           bigint,
+    isTaskEnabled     boolean,
+    parent            character varying(64),
+    projectHierarchy  text,
+    additionalDetails jsonb,
+    createdBy         character varying(64),
+    createdTime       bigint,
+    lastModifiedBy    character varying(64),
+    lastModifiedTime  bigint,
+    rowVersion        bigint,
+    isDeleted         boolean,
+    CONSTRAINT uk_project_id PRIMARY KEY (id)
 );
--- health-services/project/src/main/resources/db/migration/main/V20221208135800__project_staff_ddl.sql
+++ health-services/project/src/main/resources/db/migration/main/V20221208135800__project_staff_ddl.sql
@@ -1,16 +1,17 @@
 CREATE TABLE PROJECT_STAFF
 (
-    id                character varying(64) NOT NULL PRIMARY KEY,
-    tenantId          character varying(1000) NOT NULL,
-    projectId 		  character varying(64) NOT NULL,
-    staffId 		  character varying(255) NOT NULL,
+    id                character varying(64),
+    tenantId          character varying(1000),
+    projectId 		  character varying(64),
+    staffId 		  character varying(64),
     startDate         bigint,
     endDate           bigint,
-    additionalDetails text,
+    additionalDetails jsonb,
     createdBy         character varying(64),
     lastModifiedBy    character varying(64),
     createdTime       bigint,
     lastModifiedTime  bigint,
     rowVersion        bigint,
-    isDeleted         boolean
- );
+    isDeleted         boolean,
+    CONSTRAINT uk_project_staff_id PRIMARY KEY (id)
+);
