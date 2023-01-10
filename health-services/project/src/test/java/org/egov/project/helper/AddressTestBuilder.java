4877d89028c40a10756c144dd492432cdc4af80b
Vishal <sarasani.vishalreddy@egovernments.org>
2023-01-10 19:13:27 +0530

    [hlm-688]: Added test cases for project task search functionality

--- health-services/project/src/test/java/org/egov/project/helper/AddressTestBuilder.java
+++ health-services/project/src/test/java/org/egov/project/helper/AddressTestBuilder.java
@@ -2,6 +2,7 @@ package org.egov.project.helper;
 
 
 import org.egov.project.web.models.Address;
+import org.egov.project.web.models.AddressType;
 
 public class AddressTestBuilder {
     private final Address.AddressBuilder builder;
@@ -20,7 +21,7 @@ public class AddressTestBuilder {
     public AddressTestBuilder withAddress(){
         this.builder.tenantId("default").addressLine1("line 1").addressLine2("line 2").id("some-id").city("city")
                 .landmark("landmark").buildingName("buildingName").latitude(12.31).longitude(12.31)
-                .locationAccuracy(12.21).pincode("98909").type("HOME");
+                .locationAccuracy(12.21).pincode("98909").type(AddressType.fromValue("HOME"));
         return this;
     }
 }
