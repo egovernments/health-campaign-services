854370243b091a6af80d23ffcdebcbbaa6afcdf0
jitendrachoudhary <jitendra.choudhary@egovernments.org>
2023-01-27 08:54:52 +0530

    Updated the update code for failing scenario

--- health-services/household/src/test/java/org/egov/household/service/HouseholdUpdateTest.java
+++ health-services/household/src/test/java/org/egov/household/service/HouseholdUpdateTest.java
@@ -100,6 +100,7 @@ class HouseholdUpdateTest {
     @Test
     @DisplayName("should send data to kafka topic")
     void shouldSendDataToKafkaTopic() {
+
         HouseholdRequest request = HouseholdRequestTestBuilder.builder().withHousehold().withRequestInfo()
                 .withApiOperationDelete().build();
         when(householdRepository.findById(anyList(), eq("id"), eq(false)))
