3c9b607c0bdd766e50c887a38b653f74319426cf
jayant.porwal <jayant.porwal@egovernments.org>
2023-02-25 01:33:54 +0530

    [hlm-1919]: Changed deserializer and topic name - Removed redundant loggers

--- health-services/transformer/src/main/java/org/egov/transformer/consumer/ProjectTaskConsumer.java
+++ health-services/transformer/src/main/java/org/egov/transformer/consumer/ProjectTaskConsumer.java
@@ -1,8 +1,8 @@
 package org.egov.transformer.consumer;
 
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.egov.transformer.enums.Operation;
 import org.egov.transformer.handler.TransformationHandler;
 import org.egov.transformer.models.upstream.Task;
@@ -13,8 +13,8 @@ import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 
 @Component
 @Slf4j
@@ -32,11 +32,12 @@ public class ProjectTaskConsumer {
     }
 
     @KafkaListener(topics = "${transformer.consumer.bulk.create.project.task.topic}")
-    public void bulkCreate(Map<String, Object> consumerRecord,
-                                       @Header(KafkaHeaders.RECEIVED_TOPIC) String topic) {
+    public void bulkCreate(ConsumerRecord<String, Object> payload,
+                           @Header(KafkaHeaders.RECEIVED_TOPIC) String topic) {
         try {
-            log.info("payload(s) received on topic {}", topic);
-            List<Task> payloadList = objectMapper.convertValue(consumerRecord, new TypeReference<List<Task>>(){});
+            List<Task> payloadList = Arrays.asList(objectMapper
+                    .readValue((String) payload.value(),
+                            Task[].class));
             transformationHandler.handle(payloadList, Operation.CREATE);
         } catch (Exception exception) {
             log.error("error in project task consumer bulk create", exception);
--- health-services/transformer/src/main/java/org/egov/transformer/producer/Producer.java
+++ health-services/transformer/src/main/java/org/egov/transformer/producer/Producer.java
@@ -3,7 +3,6 @@ package org.egov.transformer.producer;
 import lombok.extern.slf4j.Slf4j;
 import org.egov.tracer.kafka.CustomKafkaTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
 
 // NOTE: If tracer is disabled change CustomKafkaTemplate to KafkaTemplate in autowiring
@@ -20,7 +19,6 @@ public class Producer {
     }
 
     public void push(String topic, Object value) {
-        SendResult<String, Object> result = kafkaTemplate.send(topic, value);
-        log.info("pushed to kafka {}", result.getRecordMetadata());
+        kafkaTemplate.send(topic, value);
     }
 }
