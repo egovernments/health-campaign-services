c4694876df41ce441cee45be2e4bd6567d32051b
saiprakash-egov <sai.prakash@egovernments.org>
2023-03-09 11:26:51 +0530

    removing unused import

--- core-services/dashboard-analytics/src/main/java/com/tarento/analytics/org/service/TarentoServiceImpl.java
+++ core-services/dashboard-analytics/src/main/java/com/tarento/analytics/org/service/TarentoServiceImpl.java
@@ -10,7 +10,6 @@ import java.util.concurrent.TimeUnit;
 
 import com.tarento.analytics.dto.*;
 import org.apache.commons.lang3.StringUtils;
-import org.elasticsearch.common.recycler.Recycler.C;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
