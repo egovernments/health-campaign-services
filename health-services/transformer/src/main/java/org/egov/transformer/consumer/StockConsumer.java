caacdc45e7351c319e106c392428adf39eeff203
Menka-egov <menka.gorakhapuriya@egovernments.org>
2023-03-09 12:49:21 +0530

    [hlm-2009]: Added transformation handler

--- health-services/transformer/src/main/java/org/egov/transformer/consumer/StockConsumer.java
+++ health-services/transformer/src/main/java/org/egov/transformer/consumer/StockConsumer.java
@@ -4,9 +4,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.egov.transformer.enums.Operation;
-import org.egov.transformer.handler.StockTransformationHandler;
 import org.egov.transformer.handler.TransformationHandler;
 import org.egov.transformer.models.upstream.Stock;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
@@ -20,22 +20,24 @@ public class StockConsumer {
 
     private final ObjectMapper objectMapper;
 
-    private final TransformationHandler<StockTransformationHandler> transformationHandler;
+    private final TransformationHandler<Stock> transformationHandler;
 
-    public StockConsumer(ObjectMapper objectMapper, TransformationHandler<StockTransformationHandler> transformationHandler) {
+    public StockConsumer(ObjectMapper objectMapper, TransformationHandler<Stock> transformationHandler) {
         this.objectMapper = objectMapper;
         this.transformationHandler = transformationHandler;
     }
 
+    @KafkaListener(topics = { "${transformer.consumer.bulk.create.stock.topic}",
+            "${transformer.consumer.bulk.update.stock.topic}"})
     public void bulkCreate(ConsumerRecord<String, Object> payload,
                            @Header(KafkaHeaders.RECEIVED_TOPIC) String topic) {
         try {
             List<Stock> payloadList = Arrays.asList(objectMapper
                     .readValue((String) payload.value(),
                             Stock[].class));
-            transformationHandler.handle(payloadList, Operation.TASK);
+            transformationHandler.handle(payloadList, Operation.STOCK);
         } catch (Exception exception) {
-            log.error("error in project task consumer bulk create", exception);
+            log.error("error in stock consumer bulk create", exception);
         }
     }
 }
--- health-services/transformer/src/main/java/org/egov/transformer/handler/StockTransformationHandler.java
+++ health-services/transformer/src/main/java/org/egov/transformer/handler/StockTransformationHandler.java
@@ -3,15 +3,33 @@ package org.egov.transformer.handler;
 import lombok.extern.slf4j.Slf4j;
 import org.egov.transformer.enums.Operation;
 import org.egov.transformer.models.upstream.Stock;
+import org.egov.transformer.service.TransformationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 
 @Component
 @Slf4j
 public class StockTransformationHandler implements TransformationHandler<Stock> {
+
+    private final Map<Operation, List<TransformationService<Stock>>> operationTransformationServiceMap;
+
+    @Autowired
+    public StockTransformationHandler(@Qualifier("stockTransformationServiceMap")
+                                     Map<Operation, List<TransformationService<Stock>>> operationTransformationServiceMap) {
+        this.operationTransformationServiceMap = operationTransformationServiceMap;
+    }
+
     @Override
     public void handle(List<Stock> payloadList, Operation operation) {
-
+        operationTransformationServiceMap.entrySet().stream()
+                .filter(e -> e.getKey().equals(operation))
+                .map(Map.Entry::getValue)
+                .flatMap(Collection::stream).forEach(es -> es.transform(payloadList));
     }
+
 }
