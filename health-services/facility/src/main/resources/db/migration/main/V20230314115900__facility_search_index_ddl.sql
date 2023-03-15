121f32e7b3bb400b4e1173eab253265641be1ebf
Shiva Burade <shiva.burade@egovernments.org>
2023-03-15 16:15:00 +0530

    [hlm-2031]: Updated indexes for facility service

--- health-services/facility/src/main/resources/db/migration/main/V20230314115900__facility_search_index_ddl.sql
+++ health-services/facility/src/main/resources/db/migration/main/V20230314115900__facility_search_index_ddl.sql
@@ -1,6 +1,6 @@
-CREATE INDEX idx_id on FACILITY (id);
-CREATE INDEX idx_isPermanent on FACILITY (isPermanent);
-CREATE INDEX idx_usage on FACILITY (usage);
-CREATE INDEX idx_storageCapacity on FACILITY (storageCapacity);
+CREATE INDEX idx_facility_id on FACILITY (id);
+CREATE INDEX idx_facility_isPermanent on FACILITY (isPermanent);
+CREATE INDEX idx_facility_usage on FACILITY (usage);
+CREATE INDEX idx_facility_storageCapacity on FACILITY (storageCapacity);
 
 CREATE INDEX IF NOT EXISTS idx_localityCode ON ADDRESS (localityCode);
