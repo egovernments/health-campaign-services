f9c86cd6236c3d55bbe917b89c994c70ead672c2
Vishal <sarasani.vishalreddy@egovernments.org>
2023-05-14 18:07:06 +0530

    [hlm-1834]: debug transformer changes for boundary

--- health-services/transformer/src/main/java/org/egov/transformer/service/ProjectTaskTransformationService.java
+++ health-services/transformer/src/main/java/org/egov/transformer/service/ProjectTaskTransformationService.java
@@ -66,7 +66,7 @@ public abstract class ProjectTaskTransformationService implements Transformation
         @Override
         public List<ProjectTaskIndexV1> transform(Task task) {
             Map<String, String> boundaryLabelToNameMap = projectService
-                    .getBoundaryLabelToNameMap(task.getAddress().getBoundary(), task.getTenantId());
+                    .getBoundaryLabelToNameMap(task.getAddress().getLocality().getCode(), task.getTenantId());
             log.info("boundary labels {}", boundaryLabelToNameMap.toString());
             return task.getResources().stream().map(r ->
                     ProjectTaskIndexV1.builder()
