e96556c147ec402c9a243442d7c2864383de8f86
saiprakash-egov <sai.prakash@egovernments.org>
2023-07-20 17:11:20 +0530

    adding dob to user request and removing familyname

--- health-services/individual/src/main/java/org/egov/individual/service/IndividualMapper.java
+++ health-services/individual/src/main/java/org/egov/individual/service/IndividualMapper.java
@@ -31,8 +31,8 @@ public class IndividualMapper {
                 .id(id)
                 .uuid(individual.getUserUuid())
                 .tenantId(individual.getTenantId())
-                .name(String.join(" ", individual.getName().getGivenName(),
-                        individual.getName().getFamilyName()))
+                .name(individual.getName().getGivenName())
+                .dob(individual.getDateOfBirth())
                 .correspondenceAddress(addressLine1)
                 .emailId(individual.getEmail())
                 .mobileNumber(generateDummyMobileNumber(individual.getMobileNumber()))
