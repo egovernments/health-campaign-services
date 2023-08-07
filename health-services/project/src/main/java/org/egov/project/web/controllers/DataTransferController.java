207d94719e31fabc10d45adfb920e443949c836d
kavi_elrey <25226238+kavi-egov@users.noreply.github.com>
2023-08-07 12:54:46 +0530

    added data dump api

--- health-services/project/src/main/java/org/egov/project/web/controllers/DataTransferController.java
+++ health-services/project/src/main/java/org/egov/project/web/controllers/DataTransferController.java
@@ -3,8 +3,11 @@ package org.egov.project.web.controllers;
 import io.swagger.annotations.ApiParam;
 import lombok.extern.slf4j.Slf4j;
 import org.egov.common.utils.ResponseInfoFactory;
+import org.egov.project.util.ErrorDumpUtil;
 import org.egov.project.web.models.BandwidthCheckRequest;
 import org.egov.project.web.models.BandwidthCheckResponse;
+import org.egov.project.web.models.DataErrorDumpRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -19,7 +22,10 @@ import javax.validation.Valid;
 @RequestMapping("")
 @Validated
 @Slf4j
-public class BandwidthController {
+public class DataTransferController {
+	
+	@Autowired
+	private ErrorDumpUtil errorDumpUtil;
 
 
     @RequestMapping(value = "/check/bandwidth", method = RequestMethod.POST)
@@ -29,6 +35,19 @@ public class BandwidthController {
                 .responseInfo(ResponseInfoFactory
                 .createResponseInfo(request.getRequestInfo(), true))
                 .additionalFields(request.getAdditionalFields())
+                .build());
+    }
+    
+    @RequestMapping(value = "/data/recoveryindex", method = RequestMethod.POST)
+    public ResponseEntity<BandwidthCheckResponse> indexRecoveryData(@ApiParam(value = "Captures failed data from offline client to index", required = true) @Valid @RequestBody DataErrorDumpRequest request) {
+        log.info("Request received: {}", request);
+        
+        
+        errorDumpUtil.sendErrorsToQueue(request);
+        return ResponseEntity.status(HttpStatus.OK).body(BandwidthCheckResponse.builder()
+                .responseInfo(ResponseInfoFactory
+                .createResponseInfo(request.getRequestInfo(), true))
+                .additionalFields(null)
                 .build());
     }
 }
