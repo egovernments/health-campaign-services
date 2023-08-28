fcfb558675e9d30151e7a77601a311d9e22a1097
kavi_elrey@1993 <25226238+kavi-egov@users.noreply.github.com>
2023-08-29 04:17:40 +0530

    Update Address.java

--- health-services/libraries/health-services-models/src/main/java/org/egov/common/models/facility/Address.java
+++ health-services/libraries/health-services-models/src/main/java/org/egov/common/models/facility/Address.java
@@ -51,7 +51,6 @@ public class Address {
 
     @JsonProperty("locationAccuracy")
     @DecimalMin("0")
-    @DecimalMax("10000")
     private Double locationAccuracy = null;
 
     @JsonProperty("type")
