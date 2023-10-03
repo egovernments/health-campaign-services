fd2a80b1b5393dc84c2d345879267ba5b119a924
shubhang-egov <shubhang.dhanesha@egovernments.org>
2023-10-03 10:28:16 +0530

    PFM-4777-added slf4j and log statement

--- health-services/individual/src/main/java/org/egov/individual/validators/SearchValidator.java
+++ health-services/individual/src/main/java/org/egov/individual/validators/SearchValidator.java
@@ -1,11 +1,16 @@
 package org.egov.individual.validators;
 
+import lombok.extern.slf4j.Slf4j;
 import org.egov.individual.web.models.IndividualSearch;
 import org.egov.tracer.model.CustomException;
+import org.springframework.stereotype.Component;
 
+@Component
+@Slf4j
 public class SearchValidator {
 
     public void validate (IndividualSearch individualSearch) {
+        log.info("Validating search call");
         if (individualSearch.getCreatedFrom() != null && individualSearch.getCreatedTo() != null &&
         individualSearch.getCreatedFrom().compareTo(individualSearch.getCreatedTo()) > 0) {
             throw new CustomException("FROM_GREATER_THAN_TO_DATE", "From date is greater than to date");
