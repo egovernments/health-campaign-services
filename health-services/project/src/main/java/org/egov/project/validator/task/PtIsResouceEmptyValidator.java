a0e237d4ae0663ff3c64594d06a1b3d6d3113f00
kanishq-egov <kanishq.bhatnagar@egovernments.org>
2023-10-13 15:44:06 +0530

    HLM-3073: updated code comments

--- health-services/project/src/main/java/org/egov/project/validator/task/PtIsResouceEmptyValidator.java
+++ health-services/project/src/main/java/org/egov/project/validator/task/PtIsResouceEmptyValidator.java
@@ -25,13 +25,21 @@ import static org.egov.project.util.ProjectConstants.TASK_NOT_ALLOWED;
 @Order(value = 1)
 @Slf4j
 public class PtIsResouceEmptyValidator implements Validator<TaskBulkRequest, Task> {
+
+    /**
+     *  Returns all the invalid objects in the request based on the task resources.
+     * @param request of TaskBulkRequest class
+     * @return errorDetailsMap
+     */
     public Map<Task, List<Error>> validate(TaskBulkRequest request) {
         Map<Task, List<Error>> errorDetailsMap = new HashMap<>();
         List<Task> entities = request.getTasks();
         if(!entities.isEmpty()) {
             entities.forEach(task -> {
                 if(CollectionUtils.isEmpty(task.getResources()) && !ProjectConstants.TaskStatus.BENEFICIARY_REFUSED.toString().equals(task.getStatus())) {
-                    // if the task resource is empty or null and task status is not BENEFICIARY_REFUSED
+                    /**
+                     *  If the task resource is empty or null and task status is not BENEFICIARY_REFUSED it is invalid
+                     */
                     Error error = Error.builder()
                             .errorMessage(ProjectConstants.TASK_NOT_ALLOWED_RESOURCE_CANNOT_EMPTY_ERROR_MESSAGE + ProjectConstants.TaskStatus.BENEFICIARY_REFUSED)
                             .errorCode(TASK_NOT_ALLOWED)
@@ -39,7 +47,9 @@ public class PtIsResouceEmptyValidator implements Validator<TaskBulkRequest, Tas
                             .exception(new CustomException(TASK_NOT_ALLOWED, ProjectConstants.TASK_NOT_ALLOWED_RESOURCE_CANNOT_EMPTY_ERROR_MESSAGE + ProjectConstants.TaskStatus.BENEFICIARY_REFUSED)).build();
                     populateErrorDetails(task, error, errorDetailsMap);
                 } else if (!CollectionUtils.isEmpty(task.getResources()) && ProjectConstants.TaskStatus.BENEFICIARY_REFUSED.toString().equals(task.getStatus())) {
-                    // If the task resource is not empty and task status is not BENEFICIARY_REFUSED
+                    /**
+                     *  If the task resource is not empty and task status is BENEFICIARY_REFUSED
+                      */
                     Error error = Error.builder()
                             .errorMessage(ProjectConstants.TASK_NOT_ALLOWED_BENEFICIARY_REFUSED_RESOURCE_EMPTY_ERROR_MESSAGE)
                             .errorCode(TASK_NOT_ALLOWED)
