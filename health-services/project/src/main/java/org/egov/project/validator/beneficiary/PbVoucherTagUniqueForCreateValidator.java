99a072f9f69f9d79e7c02604278355e6282d289a
kanishq-egov <kanishq.bhatnagar@egovernments.org>
2023-10-30 14:59:41 +0530

    Added and updated for unique field voucher tag create and update scenario

--- health-services/project/src/main/java/org/egov/project/validator/beneficiary/PbVoucherTagUniqueForCreateValidator.java
+++ health-services/project/src/main/java/org/egov/project/validator/beneficiary/PbVoucherTagUniqueForCreateValidator.java
@@ -6,12 +6,12 @@ import org.egov.common.models.project.BeneficiaryBulkRequest;
 import org.egov.common.models.project.ProjectBeneficiary;
 import org.egov.common.validator.Validator;
 import org.egov.project.repository.ProjectBeneficiaryRepository;
-import org.egov.project.service.ProjectBeneficiaryService;
 import org.egov.project.web.models.ProjectBeneficiarySearch;
 import org.egov.tracer.model.CustomException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -22,7 +22,7 @@ import java.util.stream.Collectors;
 
 import static org.egov.common.utils.CommonUtils.notHavingErrors;
 import static org.egov.common.utils.CommonUtils.populateErrorDetails;
-import static org.egov.common.utils.ValidatorUtils.getErrorForNonExistentEntity;
+import static org.egov.common.utils.ValidatorUtils.getErrorForUniqueEntity;
 
 /**
  * This class, PbVoucherTagUniqueValidator, is a Spring component that serves as a validator for ensuring the uniqueness
@@ -33,11 +33,11 @@ import static org.egov.common.utils.ValidatorUtils.getErrorForNonExistentEntity;
 @Component
 @Order(value = 2)
 @Slf4j
-public class PbVoucherTagUniqueValidator implements Validator<BeneficiaryBulkRequest, ProjectBeneficiary> {
+public class PbVoucherTagUniqueForCreateValidator implements Validator<BeneficiaryBulkRequest, ProjectBeneficiary> {
     final ProjectBeneficiaryRepository projectBeneficiaryRepository;
 
     @Autowired
-    public PbVoucherTagUniqueValidator(ProjectBeneficiaryRepository projectBeneficiaryRepository) {
+    public PbVoucherTagUniqueForCreateValidator(ProjectBeneficiaryRepository projectBeneficiaryRepository) {
         this.projectBeneficiaryRepository = projectBeneficiaryRepository;
     }
 
@@ -48,7 +48,7 @@ public class PbVoucherTagUniqueValidator implements Validator<BeneficiaryBulkReq
      */
     @Override
     public Map<ProjectBeneficiary, List<Error>> validate(BeneficiaryBulkRequest beneficiaryBulkRequest) {
-        log.info("validating unique tag");
+        log.info("validating unique tag for create");
 
         // Create a map to store error details for each ProjectBeneficiary
         Map<ProjectBeneficiary, List<Error>> errorDetailsMap = new HashMap<>();
@@ -59,62 +59,59 @@ public class PbVoucherTagUniqueValidator implements Validator<BeneficiaryBulkReq
 
         if(!validProjectBeneficiaries.isEmpty()) {
             // Get a list of invalid voucher tags
-            List<String> invalidVoucherTags = getInvalidVoucherTags(validProjectBeneficiaries, beneficiaryBulkRequest);
+            List<ProjectBeneficiary> existingProjectBeneficiaries = getInvalidVoucherTags(validProjectBeneficiaries);
 
             // Validate and populate errors for invalid voucher tags
-            validateAndPopulateErrors(validProjectBeneficiaries, invalidVoucherTags, errorDetailsMap);
+            if(!existingProjectBeneficiaries.isEmpty())
+                validateAndPopulateErrors(validProjectBeneficiaries, existingProjectBeneficiaries, errorDetailsMap);
         }
 
         return errorDetailsMap;
     }
 
     // Helper method to validate and populate errors
-    private void validateAndPopulateErrors(List<ProjectBeneficiary> validProjectBeneficiaries, List<String> invalidVoucherTags, Map<ProjectBeneficiary, List<Error>> errorDetailsMap) {
-
+    private void validateAndPopulateErrors(List<ProjectBeneficiary> validProjectBeneficiaries, List<ProjectBeneficiary> existingProjectBeneficiaries, Map<ProjectBeneficiary, List<Error>> errorDetailsMap) {
         // Filter project beneficiaries that are valid and have invalid voucher tags
         List<ProjectBeneficiary> invalidEntities = validProjectBeneficiaries.stream().filter(notHavingErrors())
-                .filter(entity -> invalidVoucherTags.contains(entity.getVoucherTag()))
+                .filter(entity -> existingProjectBeneficiaries.contains(entity.getVoucherTag()))
                 .collect(Collectors.toList());
 
         // For each invalid entity, create an error and populate error details
         invalidEntities.forEach(projectBeneficiary -> {
-            Error error = getErrorForNonExistentEntity();
+            Error error = getErrorForUniqueEntity();
             populateErrorDetails(projectBeneficiary, error, errorDetailsMap);
         });
     }
 
     // Helper method to get invalid voucher tags
-    private List<String> getInvalidVoucherTags(List<ProjectBeneficiary> validProjectBeneficiaries, BeneficiaryBulkRequest beneficiaryBulkRequest) {
+    private List<ProjectBeneficiary> getInvalidVoucherTags(List<ProjectBeneficiary> validProjectBeneficiaries) {
         // Extract voucher tags from valid project beneficiaries
         List<String> voucherTags = validProjectBeneficiaries.stream()
                 .filter(Objects::nonNull)
                 .map(ProjectBeneficiary::getVoucherTag)
                 .collect(Collectors.toList());
 
-        // Create a list to store invalid voucher tags
-        List<String> invalidVoucherTags;
+        if(CollectionUtils.isEmpty(voucherTags))
+            return new ArrayList<>();
+
+        // Create a list to store existing project beneficiary with voucher tag
+        List<ProjectBeneficiary> existingProjectBeneficiaries;
 
         // Build a search request to find existing voucher tags
         ProjectBeneficiarySearch projectBeneficiarySearch = ProjectBeneficiarySearch.builder().voucherTag(voucherTags).build();
+
         try {
             log.info("Fetching project beneficiary based on voucher tags");
-            List<String> existingVoucherTags = projectBeneficiaryRepository.find(
-                        projectBeneficiarySearch,
-                        voucherTags.size(), 0, validProjectBeneficiaries.get(0).getTenantId(), null, false
-                    )
-                    .stream()
-                    .map(ProjectBeneficiary::getVoucherTag)
-                    .collect(Collectors.toList());
-
-            // Remove existing voucher tags to get invalid ones
-            voucherTags.removeAll(existingVoucherTags);
+            existingProjectBeneficiaries = projectBeneficiaryRepository.find(
+                    projectBeneficiarySearch,
+                    voucherTags.size(), 0, validProjectBeneficiaries.get(0).getTenantId(), null, false
+            );
         } catch (Exception e) {
             log.error("Exception while fetching project beneficiary service : ", e);
             throw new CustomException("PROJECT_BENEFICIARY_VOUCHER_TAG_SEARCH_FAILED","Error occurred while fetching project beneficiary based on voucher tags. "+e);
         }
 
-        // Copy the invalid voucher tags to the result list
-        invalidVoucherTags = new ArrayList<>(voucherTags);
-        return invalidVoucherTags;
+        // return existing project beneficiaries
+        return existingProjectBeneficiaries;
     }
 }
