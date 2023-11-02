e5497aef33477741051304160be70b4d8c1f678a
prerna-egov <prerna.kumari@egovernments.org>
2023-11-02 15:18:29 +0530

    made base workbench-hcm module

---
+++ micro-ui/web/micro-ui-internals/packages/modules/workbench-hcm/src/Module.js
@@ -0,0 +1,86 @@
+import { Loader, TourProvider } from "@egovernments/digit-ui-react-components";
+import React from "react";
+import { useRouteMatch } from "react-router-dom";
+import EmployeeApp from "./pages/employee";
+import { CustomisedHooks } from "./hooks";
+import { UICustomizations } from "./configs/UICustomizations";
+import WorkbenchHCMCard from "./components/WorkbenchHCMCard";
+import * as parsingUtils from "../src/utils/ParsingUtils";
+
+const WorkbenchHCMModule = ({ stateCode, userType, tenants }) => {
+  const moduleCode = ["workbench-hcm", "mdms", "schema"];
+  const { path, url } = useRouteMatch();
+  const language = Digit.StoreData.getCurrentLanguage();
+  const { isLoading, data: store } = Digit.Services.useStore({
+    stateCode,
+    moduleCode,
+    language,
+  });
+
+  if (isLoading) {
+    return <Loader />;
+  }
+
+  return (
+    <TourProvider>
+      <EmployeeApp path={path} stateCode={stateCode} />
+    </TourProvider>
+  );
+};
+
+const componentsToRegister = {
+  WorkbenchHCMModule,
+  WorkbenchHCMCard,
+};
+
+const overrideHooks = () => {
+  Object.keys(CustomisedHooks).map((ele) => {
+    if (ele === "Hooks") {
+      Object.keys(CustomisedHooks[ele]).map((hook) => {
+        Object.keys(CustomisedHooks[ele][hook]).map((method) => {
+          setupHooks(hook, method, CustomisedHooks[ele][hook][method]);
+        });
+      });
+    } else if (ele === "Utils") {
+      Object.keys(CustomisedHooks[ele]).map((hook) => {
+        Object.keys(CustomisedHooks[ele][hook]).map((method) => {
+          setupHooks(hook, method, CustomisedHooks[ele][hook][method], false);
+        });
+      });
+    } else {
+      Object.keys(CustomisedHooks[ele]).map((method) => {
+        setupLibraries(ele, method, CustomisedHooks[ele][method]);
+      });
+    }
+  });
+};
+
+/* To Overide any existing hook we need to use similar method */
+const setupHooks = (HookName, HookFunction, method, isHook = true) => {
+  window.Digit = window.Digit || {};
+  window.Digit[isHook ? "Hooks" : "Utils"] = window.Digit[isHook ? "Hooks" : "Utils"] || {};
+  window.Digit[isHook ? "Hooks" : "Utils"][HookName] = window.Digit[isHook ? "Hooks" : "Utils"][HookName] || {};
+  window.Digit[isHook ? "Hooks" : "Utils"][HookName][HookFunction] = method;
+};
+/* To Overide any existing libraries  we need to use similar method */
+const setupLibraries = (Library, service, method) => {
+  window.Digit = window.Digit || {};
+  window.Digit[Library] = window.Digit[Library] || {};
+  window.Digit[Library][service] = method;
+};
+
+/* To Overide any existing config/middlewares  we need to use similar method */
+const updateCustomConfigs = () => {
+  setupLibraries("Customizations", "commonUiConfig", { ...window?.Digit?.Customizations?.commonUiConfig, ...UICustomizations });
+  setupLibraries("Utils", "parsingUtils", { ...window?.Digit?.Utils?.parsingUtils, ...parsingUtils });
+};
+
+const initWorkbenchHCMComponents = () => {
+  overrideHooks();
+  updateCustomConfigs();
+  Object.entries(componentsToRegister).forEach(([key, value]) => {
+    Digit.ComponentRegistryService.setComponent(key, value);
+  });
+};
+
+export { initWorkbenchHCMComponents };
---
+++ micro-ui/web/micro-ui-internals/packages/modules/workbench-hcm/src/pages/employee/index.js
@@ -0,0 +1,55 @@
+import React, { useEffect } from "react";
+import { Switch, useLocation } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+import { PrivateRoute, AppContainer, BreadCrumb } from "@egovernments/digit-ui-react-components";
+
+const WorkbenchBreadCrumb = ({ location ,defaultPath}) => {
+  const { t } = useTranslation();
+  const search = useLocation().search;
+  const fromScreen = new URLSearchParams(search).get("from") || null;
+  const pathVar=location.pathname.replace(defaultPath+'/',"").split("?")?.[0];
+  const {masterName,moduleName,uniqueIdentifier} = Digit.Hooks.useQueryParams()
+
+  const crumbs = [
+    {
+      path: `/${window?.contextPath}/employee`,
+      content: t("WORKBENCH_HOME"),
+      show: true,
+    },
+    
+  ];
+  return <BreadCrumb className="workbench-bredcrumb" crumbs={crumbs} spanStyle={{ maxWidth: "min-content" }} />;
+};
+
+const App = ({ path }) => {
+  const location = useLocation();
+  const MDMSCreateSession = Digit.Hooks.useSessionStorage("MDMS_add", {});
+  const [sessionFormData, setSessionFormData, clearSessionFormData] = MDMSCreateSession;
+  
+  const MDMSViewSession = Digit.Hooks.useSessionStorage("MDMS_view", {});
+  const [sessionFormDataView,setSessionFormDataView,clearSessionFormDataView] = MDMSViewSession
+
+  useEffect(() => {
+    if (!window.location.href.includes("mdms-add-v2") && sessionFormData && Object.keys(sessionFormData) != 0) {
+      clearSessionFormData();
+    }
+    if (!window.location.href.includes("mdms-view") && sessionFormDataView ) {
+      clearSessionFormDataView();
+    }
+  }, [location]);
+
+  return (
+    <React.Fragment>
+      <div className="wbh-header-container">
+        <WorkbenchBreadCrumb location={location} defaultPath={path} />
+      </div>
+      <Switch>
+        <AppContainer className="workbench">
+          <PrivateRoute path={`${path}/sample`} component={() => <div>Sample Screen loaded</div>} />
+        </AppContainer>
+      </Switch>
+    </React.Fragment>
+  );
+};
+
+export default App;
