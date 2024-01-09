fc4d611f681e42a561122706e8699bccbb2b5eff
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2024-01-09 11:27:39 +0530

    Update HCMWORKBENCHCard.js

--- micro-ui/web/micro-ui-internals/packages/modules/workbench-hcm/src/components/HCMWORKBENCHCard.js
+++ micro-ui/web/micro-ui-internals/packages/modules/workbench-hcm/src/components/HCMWORKBENCHCard.js
@@ -7,6 +7,7 @@ const ROLES = {
   LOCALISATION: ["EMPLOYEE", "SUPERUSER", "EMPLOYEE_COMMON", "LOC_ADMIN", "SYSTEM_ADMINISTRATOR"],
   MDMS: ["MDMS_ADMIN", "EMPLOYEE", "SUPERUSER", "SYSTEM_ADMINISTRATOR"],
   DSS: ["STADMIN", "SYSTEM_ADMINISTRATOR"],
+  HCM:["SYSTEM_ADMINISTRATOR","CAMPAIGN_ADMIN"]
 };
 
 // Mukta Overrriding the Works Home screen card
@@ -20,59 +21,80 @@ const HCMWORKBENCHCard = () => {
 
   let links = [
     {
-      label: t("WORKBENCH_INBOX"),
-      link: `/${window?.contextPath}/employee/hcmworkbench/inbox`,
-      roles: ROLES.MDMS,
+      label: t("WORKBENCH_HELP_SCREEN_BASIC_SETUP"),
+      link: `/${window?.contextPath}/employee/hcmworkbench/help-screen/basic-setup`,
+      roles: ROLES.HCM,
     },
     {
-      label: t("WORKBENCH_FACILITY"),
-      link: `/${window?.contextPath}/employee/hcmworkbench/facility`,
-      roles: ROLES.MDMS,
-    },
-    {
-      label: t("WORKBENCH_USER"),
-      link: `/${window?.contextPath}/employee/hcmworkbench/user`,
-      roles: ROLES.MDMS,
-    },
-    {
-      label: t("WORKBENCH_BOUNDARY"),
-      link: `/${window?.contextPath}/employee/hcmworkbench/boundary`,
-      roles: ROLES.MDMS,
-    },
-    {
-      label: t("WORKBENCH_PROJECT"),
-      link: `/${window?.contextPath}/employee/hcmworkbench/project`,
-      roles: ROLES.MDMS,
-    },
-    {
-      label: t("WORKBENCH_CREATE_CAMPAIGN"),
-      link: `/${window?.contextPath}/employee/hcmworkbench/campaign`,
-    },
-      {
       label: t("WORKBENCH_MASTER"),
       link: `/${window?.contextPath}/employee/hcmworkbench/master/master-landing-screen`,
-      roles: ROLES.MDMS,
+      roles: ROLES.HCM,
+    },
+    {
+      label: t("ACTION_TEST_LOCALISATION"),
+      link: `/${window?.contextPath}/employee/workbench/localisation-search`,
+      roles: ROLES.HCM,
     },
     {
       label: t("WORKBENCH_USER"),
       link: `/${window?.contextPath}/employee/hcmworkbench/master/user-landing-screen`,
-      roles: ROLES.MDMS,
-    },
-    {
-      label: t("WORKBENCH_HELP_SCREEN_BASIC_SETUP"),
-      link: `/${window?.contextPath}/employee/hcmworkbench/help-screen/basic-setup`,
-      roles: ROLES.MDMS,
+      roles: ROLES.HCM,
     },
     {
       label: t("WORKBENCH_HELP_SCREEN_CHECKLIST"),
       link: `/${window?.contextPath}/employee/hcmworkbench/help-screen/check-list`,
-      roles: ROLES.MDMS,
+      roles: ROLES.HCM,
     },
     {
-      label: t("WORKBENCH_HELP_SCREEN_CODED_USER"),
-      link: `/${window?.contextPath}/employee/hcmworkbench/help-screen/coded-user`,
-      roles: ROLES.MDMS,
+      label: t("WORKBENCH_INBOX"),
+      link: `/${window?.contextPath}/employee/hcmworkbench/inbox`,
+      roles: ROLES.HCM,
     },
+  //   {
+  //     label: t("WORKBENCH_CREATE_CAMPAIGN"),
+  //     link: `/${window?.contextPath}/employee/hcmworkbench/campaign`,
+  //   },
+     
+ 
+  //   {
+  //     label: t("ACTION_TEST_LOCALISATION"),
+  //     link: `/${window?.contextPath}/employee/workbench/localisation-search`,
+  //     roles: ROLES.HCM,
+  //   },
+  
+  //   {
+  //     label: t("ACTION_TEST_LOCALISATION"),
+  //     link: `/${window?.contextPath}/employee/workbench/localisation-search`,
+  //     roles: ROLES.HCM,
+  //   },
+  
+  
+  // {
+  //   label: t("WORKBENCH_HELP_SCREEN_CODED_USER"),
+  //   link: `/${window?.contextPath}/employee/hcmworkbench/help-screen/coded-user`,
+  // }
+    // {
+    //   label: t("WORKBENCH_FACILITY"),
+    //   link: `/${window?.contextPath}/employee/hcmworkbench/facility`,
+    //   roles: ROLES.HCM,
+    // },
+    // {
+    //   label: t("WORKBENCH_USER"),
+    //   link: `/${window?.contextPath}/employee/hcmworkbench/user`,
+    //   roles: ROLES.HCM,
+    // },
+    // {
+    //   label: t("WORKBENCH_BOUNDARY"),
+    //   link: `/${window?.contextPath}/employee/hcmworkbench/boundary`,
+    //   roles: ROLES.HCM,
+    // },
+    // {
+    //   label: t("WORKBENCH_PROJECT"),
+    //   link: `/${window?.contextPath}/employee/hcmworkbench/project`,
+    //   roles: ROLES.HCM,
+    // },
+   
+  
     // {
     //   label: t("WORKBENCH_MICROPLAN"),
     //   link: `/${window?.contextPath}/employee/hcmworkbench/microplan`,
