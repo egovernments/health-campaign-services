b91b9b94944e16a3db12f163465f50626b3c1b89
Vishal <sarasani.vishalreddy@egovernments.org>
2024-02-15 18:33:22 +0530

    referral changes

---
+++ health-services/referralmanagement/src/main/java/org/egov/referralmanagement/repository/rowmapper/HFReferralRowMapper.java
@@ -0,0 +1,58 @@
+package org.egov.referralmanagement.repository.rowmapper;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import digit.models.coremodels.AuditDetails;
+import org.egov.common.models.project.AdditionalFields;
+import org.egov.common.models.referralmanagement.hfreferral.HFReferral;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@Component
+public class HFReferralRowMapper implements RowMapper<HFReferral> {
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Override
+    public HFReferral mapRow(ResultSet resultSet, int i) throws SQLException {
+        try {
+            AuditDetails auditDetails = AuditDetails.builder()
+                    .createdBy(resultSet.getString("createdBy"))
+                    .createdTime(resultSet.getLong("createdTime"))
+                    .lastModifiedBy(resultSet.getString("lastModifiedBy"))
+                    .lastModifiedTime(resultSet.getLong("lastModifiedTime"))
+                    .build();
+            AuditDetails clientAuditDetails= AuditDetails.builder()
+                    .createdBy(resultSet.getString("clientCreatedBy"))
+                    .createdTime(resultSet.getLong("clientCreatedTime"))
+                    .lastModifiedBy(resultSet.getString("clientLastModifiedBy"))
+                    .lastModifiedTime(resultSet.getLong("clientLastModifiedTime"))
+                    .build();
+            return HFReferral.builder()
+                    .id(resultSet.getString("id"))
+                    .clientReferenceId(resultSet.getString("clientreferenceid"))
+                    .tenantId(resultSet.getString("tenantid"))
+                    .projectId(resultSet.getString("projectid"))
+                    .projectFacilityId(resultSet.getString("projectfacilityid"))
+                    .symptom(resultSet.getString("symptom"))
+                    .symptomSurveyId(resultSet.getString("symptomsurveyid"))
+                    .beneficiaryId(resultSet.getString("beneficiaryid"))
+                    .referralCode(resultSet.getString("referralcode"))
+                    .nationalLevelId(resultSet.getString("nationallevelid"))
+                    .additionalFields(resultSet.getString("additionalDetails") == null ? null : objectMapper
+                            .readValue(resultSet.getString("additionalDetails"), AdditionalFields.class))
+                    .rowVersion(resultSet.getInt("rowversion"))
+                    .isDeleted(resultSet.getBoolean("isdeleted"))
+                    .auditDetails(auditDetails)
+                    .clientAuditDetails(clientAuditDetails)
+                    .build();
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ health-services/referralmanagement/src/main/java/org/egov/referralmanagement/validator/hfreferral/HfrProjectFacilityIdValidator.java
@@ -0,0 +1,111 @@
+package org.egov.referralmanagement.validator.hfreferral;
+
+import lombok.extern.slf4j.Slf4j;
+import org.egov.common.http.client.ServiceRequestClient;
+import org.egov.common.models.Error;
+import org.egov.common.models.project.ProjectFacility;
+import org.egov.common.models.project.ProjectFacilityBulkResponse;
+import org.egov.common.models.project.ProjectFacilitySearch;
+import org.egov.common.models.project.ProjectFacilitySearchRequest;
+import org.egov.common.models.referralmanagement.hfreferral.HFReferral;
+import org.egov.common.models.referralmanagement.hfreferral.HFReferralBulkRequest;
+import org.egov.common.validator.Validator;
+import org.egov.referralmanagement.config.ReferralManagementConfiguration;
+import org.egov.tracer.model.CustomException;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+import static org.egov.common.utils.CommonUtils.notHavingErrors;
+import static org.egov.common.utils.CommonUtils.populateErrorDetails;
+import static org.egov.common.utils.ValidatorUtils.getErrorForNonExistentEntity;
+
+
+/**
+ * Validate whether project exist in db or not using project id for HFReferral object
+ */
+@Component
+@Order(value = 3)
+@Slf4j
+public class HfrProjectFacilityIdValidator implements Validator<HFReferralBulkRequest, HFReferral> {
+    private final ServiceRequestClient serviceRequestClient;
+    private final ReferralManagementConfiguration referralManagementConfiguration;
+
+    public HfrProjectFacilityIdValidator(ServiceRequestClient serviceRequestClient, ReferralManagementConfiguration referralManagementConfiguration) {
+        this.serviceRequestClient = serviceRequestClient;
+        this.referralManagementConfiguration = referralManagementConfiguration;
+    }
+
+    @Override
+    public Map<HFReferral, List<Error>> validate(HFReferralBulkRequest request) {
+        log.info("validating project facility id");
+        Map<HFReferral, List<Error>> errorDetailsMap = new HashMap<>();
+        List<HFReferral> entities = request.getHfReferrals();
+        Map<String, List<HFReferral>> tenantIdReferralMap = entities.stream().collect(Collectors.groupingBy(HFReferral::getTenantId));
+        tenantIdReferralMap.forEach((tenantId, hfReferralList) -> {
+            /** Get all the existing project in the hfReferral list from Project Service
+             */
+            List<ProjectFacility> existingProjectFacilities = getExistingProjects(tenantId, hfReferralList, request);
+            /** Validate project and populate error map if invalid entities are found
+             */
+            validateAndPopulateErrors(existingProjectFacilities, entities, errorDetailsMap);
+        });
+        return errorDetailsMap;
+    }
+    private void addIgnoreNull(List<String> list, String item) {
+        if(Objects.nonNull(item)) list.add(item);
+    }
+
+    private List<ProjectFacility> getExistingProjects(String tenantId, List<HFReferral> hfReferrals, HFReferralBulkRequest request) {
+        List<ProjectFacility> existingProjectFacilities = new ArrayList<>();
+        final List<String> projectFacilityIdList = new ArrayList<>();
+        hfReferrals.forEach(hfReferral -> {
+            addIgnoreNull(projectFacilityIdList, hfReferral.getProjectFacilityId());
+        });
+        if(!projectFacilityIdList.isEmpty()) {
+            ProjectFacilitySearch projectFacilitySearch = ProjectFacilitySearch.builder()
+                    .id(!projectFacilityIdList.isEmpty()? projectFacilityIdList : null)
+                    .tenantId(tenantId)
+                    .build();
+            try {
+                // using project facility search and fetching the valid ids.
+                ProjectFacilityBulkResponse projectFacilityBulkResponse = serviceRequestClient.fetchResult(
+                        new StringBuilder(referralManagementConfiguration.getProjectHost()
+                                + referralManagementConfiguration.getProjectFacilitySearchUrl()
+                                +"?limit=" + hfReferrals.size()
+                                + "&offset=0&tenantId=" + tenantId),
+                        ProjectFacilitySearchRequest.builder()
+                                .requestInfo(request.getRequestInfo())
+                                .projectFacility(projectFacilitySearch)
+                                .build(),
+                        ProjectFacilityBulkResponse.class
+                );
+                existingProjectFacilities = projectFacilityBulkResponse.getProjectFacilities();
+            } catch (Exception e) {
+                throw new CustomException("Project Facilities failed to fetch", "Exception : "+e.getMessage());
+            }
+        }
+
+        return existingProjectFacilities;
+    }
+
+    private void validateAndPopulateErrors(List<ProjectFacility> existingProjectFacilities, List<HFReferral> entities, Map<HFReferral, List<Error>> errorDetailsMap) {
+        final List<String> existingProjectFacilityIds = new ArrayList<>();
+        existingProjectFacilities.forEach(projectFacility -> {
+            existingProjectFacilityIds.add(projectFacility.getId());
+        });
+        List<HFReferral> invalidEntities = entities.stream().filter(notHavingErrors()).filter(entity ->
+                Objects.nonNull(entity.getProjectFacilityId()) && !existingProjectFacilityIds.contains(entity.getProjectFacilityId())
+        ).collect(Collectors.toList());
+        invalidEntities.forEach(hfReferral -> {
+            Error error = getErrorForNonExistentEntity();
+            populateErrorDetails(hfReferral, error, errorDetailsMap);
+        });
+    }
+}
