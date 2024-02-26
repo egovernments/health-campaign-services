77b6159dca44eb4ea71f818a74519d951da636b6
kanishq-egov <kanishq.bhatnagar@egovernments.org>
2024-02-26 11:48:02 +0530

    HLM-4496 : remove attendance module as it is moved to DIGIT-Works repository.

--- health-services/attendance/src/main/java/org/egov/util/AttendanceServiceConstants.java
+++
@@ -1,7 +0,0 @@
-package org.egov.util;
-
-public class AttendanceServiceConstants {
-    public static final String MASTER_TENANTS = "tenants";
-    public static final String MDMS_TENANT_MODULE_NAME = "tenant";
-    public static final String LIMIT_OFFSET = "?limit=1000&offset=0";
-}
--- health-services/attendance/src/main/resources/db/migration/main/V20240124104400__alter_attendance_log_table.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE eg_wms_attendance_log ADD COLUMN IF NOT EXISTS clientreferenceid character varying(256);
-ALTER TABLE eg_wms_attendance_log ADD COLUMN IF NOT EXISTS clientcreatedby character varying(256);
-ALTER TABLE eg_wms_attendance_log ADD COLUMN IF NOT EXISTS clientlastmodifiedby character varying(256);
-ALTER TABLE eg_wms_attendance_log ADD COLUMN IF NOT EXISTS clientcreatedtime bigint;
-ALTER TABLE eg_wms_attendance_log ADD COLUMN IF NOT EXISTS clientlastmodifiedtime bigint;
