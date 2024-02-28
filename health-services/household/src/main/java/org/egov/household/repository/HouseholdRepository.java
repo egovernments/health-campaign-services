b4f9a850825897e622e9132683f1740c0efec7b0
Vishal <sarasani.vishalreddy@egovernments.org>
2024-02-28 20:49:18 +0530

    household and address order change

--- health-services/household/src/main/java/org/egov/household/repository/HouseholdRepository.java
+++ health-services/household/src/main/java/org/egov/household/repository/HouseholdRepository.java
@@ -71,7 +71,7 @@ public class HouseholdRepository extends GenericRepository<Household> {
     }
 
     public Tuple<Long, List<Household>> find(HouseholdSearch searchObject, Integer limit, Integer offset, String tenantId, Long lastChangedSince, Boolean includeDeleted) throws QueryBuilderException {
-        String query = "SELECT *, a.id as aid,a.tenantid as atenantid, a.clientreferenceid as aclientreferenceid";
+        String query = "SELECT h.*, a.*, a.id as aid,a.tenantid as atenantid, a.clientreferenceid as aclientreferenceid";
         if (searchObject.getLocalityCode() != null) {
             query += " FROM (SELECT * FROM address WHERE localitycode = '" + searchObject.getLocalityCode() + "') a LEFT JOIN household h ON a.id = h.addressid";
         } else {
