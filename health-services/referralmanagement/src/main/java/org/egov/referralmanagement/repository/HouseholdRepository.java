ca0fa82dd9d41330e8c3c56b1914bd377251ceb1
kavi_elrey <25226238+kavi-egov@users.noreply.github.com>
2024-03-02 15:15:05 +0530

    null handled in count

--- health-services/referralmanagement/src/main/java/org/egov/referralmanagement/repository/HouseholdRepository.java
+++ health-services/referralmanagement/src/main/java/org/egov/referralmanagement/repository/HouseholdRepository.java
@@ -1,15 +1,15 @@
 package org.egov.referralmanagement.repository;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.egov.common.ds.Tuple;
 import org.egov.common.models.household.Household;
 import org.egov.referralmanagement.repository.rowmapper.HouseholdRowMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @Repository
 public class HouseholdRepository {
@@ -22,7 +22,7 @@ public class HouseholdRepository {
 	
 	 public Tuple<Long, List<Household>> findByView (String localityCode, Integer limit, Integer offset, String tenantId) {
 	    	
-	    	
+	    	Long totalCount = 0l;
 	    	String query = "select * from household_address_mv where localitycode=:localitycode and rank between :start and :end ";
 
 	        Map<String, Object> paramsMap = new HashMap<>();
@@ -33,7 +33,9 @@ public class HouseholdRepository {
 	        Map<String, Object> paramsMapCount = new HashMap<>();
 	        paramsMapCount.put("localitycode", localityCode);
 	        
-	        Long totalCount = Long.valueOf(namedParameterJdbcTemplate.queryForObject("select max(rank) from  household_address_mv where localitycode=:localitycode",paramsMapCount,Integer.class));
+	        Integer intCount = namedParameterJdbcTemplate.queryForObject("select max(rank) from  household_address_mv where localitycode=:localitycode",paramsMapCount,Integer.class);
+	        if(null != intCount)
+	        	totalCount = Long.valueOf(intCount);
 	        return new Tuple<>(totalCount, this.namedParameterJdbcTemplate.query(query, paramsMap, householdRowMapper));
 	    }
 }
