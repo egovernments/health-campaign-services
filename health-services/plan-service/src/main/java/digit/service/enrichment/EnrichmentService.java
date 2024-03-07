fc80c8e6a8c07fdce476f54e551f9da20bb6dd42
Priyanka-eGov <priyanka@egovernments.org>
2024-03-07 16:13:44 +0530

    HLM-5184 adding modifications for validations and enrichments.

--- health-services/plan-service/src/main/java/digit/service/enrichment/EnrichmentService.java
+++ health-services/plan-service/src/main/java/digit/service/enrichment/EnrichmentService.java
@@ -11,6 +11,7 @@ import java.util.List;
 import java.util.UUID;
 import lombok.extern.slf4j.Slf4j;
 import org.egov.common.contract.models.AuditDetails;
+import org.egov.common.utils.AuditDetailsEnrichmentUtil;
 import org.egov.common.utils.UUIDEnrichmentUtil;
 import org.egov.tracer.model.CustomException;
 import org.springframework.stereotype.Component;
@@ -33,30 +34,33 @@ public class EnrichmentService {
         if(request.getRequestInfo().getUserInfo() == null)
             throw new CustomException(USERINFO_MISSING_CODE, USERINFO_MISSING_MESSAGE);
 
-        enrichAuditDetails(request.getPlanConfiguration(), request.getRequestInfo().getUserInfo().getUuid(), Boolean.TRUE);
+        enrichAuditDetails(request, Boolean.TRUE);
     }
 
     public void enrichPlanConfiguration(PlanConfiguration planConfiguration) {
         log.info("enriching plan config with generated IDs");
+
+        // Generate id for plan configuration
         UUIDEnrichmentUtil.enrichRandomUuid(planConfiguration, "id");
-        UUIDEnrichmentUtil.enrichRandomUuid(planConfiguration.getFiles(), "id");
-        UUIDEnrichmentUtil.enrichRandomUuid(planConfiguration.getAssumptions(), "id");
-        UUIDEnrichmentUtil.enrichRandomUuid(planConfiguration.getOperations(), "id");
-        UUIDEnrichmentUtil.enrichRandomUuid(planConfiguration.getResourceMapping(), "id");
 
+        // Generate id for files
+        planConfiguration.getFiles().forEach(file -> UUIDEnrichmentUtil.enrichRandomUuid(file, "id"));
+
+        // Generate id for assumptions
+        planConfiguration.getAssumptions().forEach(assumption -> UUIDEnrichmentUtil.enrichRandomUuid(assumption, "id"));
+
+        // Generate id for operations
+        planConfiguration.getOperations().forEach(operation -> UUIDEnrichmentUtil.enrichRandomUuid(operation, "id"));
+
+        // Generate id for resource mappings
+        planConfiguration.getResourceMapping().forEach(resourceMapping -> UUIDEnrichmentUtil.enrichRandomUuid(resourceMapping, "id"));
     }
 
-    public void enrichAuditDetails(PlanConfiguration planConfiguration, String by, Boolean isCreate) {
-        Long time = System.currentTimeMillis();
-        planConfiguration.setAuditDetails(getAuditDetails(by,planConfiguration.getAuditDetails(), isCreate));
+    public void enrichAuditDetails(PlanConfigurationRequest request, Boolean isCreate) {
+        // Enrich audit details for plan configurationqq
+        PlanConfiguration planConfiguration = request.getPlanConfiguration();
+        planConfiguration.setAuditDetails(AuditDetailsEnrichmentUtil
+                .prepareAuditDetails(planConfiguration.getAuditDetails(), request.getRequestInfo(), isCreate));
     }
 
-    public AuditDetails getAuditDetails(String by, AuditDetails auditDetails, Boolean isCreate) {
-        Long time = System.currentTimeMillis();
-        if (isCreate)
-            return AuditDetails.builder().createdBy(by).lastModifiedBy(by).createdTime(time).lastModifiedTime(time).build();
-        else
-            return AuditDetails.builder().createdBy(auditDetails.getCreatedBy()).lastModifiedBy(by)
-                    .createdTime(auditDetails.getCreatedTime()).lastModifiedTime(time).build();
-    }
 }
