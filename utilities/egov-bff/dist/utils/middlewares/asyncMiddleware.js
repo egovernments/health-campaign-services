e05ed95d9ff26c55b8196506a1b40269615adf83
ashish-egov <ashish.tiwari@egovernments.org>
2024-04-08 15:57:36 +0530

    Feat : Removed dist

--- utilities/egov-bff/dist/utils/middlewares/asyncMiddleware.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var asyncMiddleware = function (fn) {
-    return function (req, res, next) {
-        Promise.resolve(fn(req, res, next))
-            .catch(next);
-    };
-};
-exports.default = asyncMiddleware;
