89c614386fd6baae476cf62bbec8c141cff2170b
Shashwat Mishra <shashwat.mishra@egovernments.org>
2024-04-16 17:08:43 +0530

    Adding auth proxy service to health repository

---
+++ analytics/auth-proxy/EnvironmentVariables.js
@@ -0,0 +1,12 @@
+const envVariables = {
+    SERVER_PORT: 
+      process.env.SERVER_PORT || 8085,
+    EGOV_USER_HOST:
+      process.env.EGOV_USER_HOST || "http://localhost:8081/",
+    EGOV_USER_SEARCH:
+      process.env.EGOV_USER_SEARCH || "user/_details",
+    KIBANA_HOST:
+      process.env.KIBANA_HOST || "http://localhost:30001/kibana"
+  };
+  
+  module.exports = envVariables;
---
+++ analytics/auth-proxy/server.js
@@ -0,0 +1,71 @@
+const envVariables = require('./EnvironmentVariables');
+const logger = require('./logger');
+const express = require('express')
+const proxy = require('express-http-proxy')
+const axios = require('axios');
+const deasync = require('deasync');
+const app = express()
+
+let serverPort = envVariables.SERVER_PORT;
+let kibanaHost = envVariables.KIBANA_HOST;
+
+// Authenticate token
+function authenticateToken(token) {
+    const url = envVariables.EGOV_USER_HOST + envVariables.EGOV_USER_SEARCH;
+    const queryParams = { access_token: token };
+    let result;
+
+    // try {
+    //     const response = axios.post(url, null, { params: queryParams });
+    //     logger.info(response);
+    //     // Assuming the authentication is successful if the request is successful
+    //     return response.status === 200;
+    // } catch (error) {
+    //     // Log and handle any errors
+    //     console.error('Error during authentication:', error.response.data);
+    //     return false; // Return false if authentication fails
+    // }
+
+    axios.post(url, null, { params: queryParams })
+        .then(response => {
+            result = response.status === 200;
+        })
+        .catch(error => {
+            console.error('Error during authentication:', error.response.data);
+            result = false;
+        });
+
+    // Synchronously wait until the API call completes
+    deasync.loopWhile(() => result === undefined);
+
+    return result;
+}
+
+// Intercept Kibana requests, extract auth token and perform authentication
+app.use((req, res, next) => {
+
+    logger.info("Received request");
+
+    // Extract auth token from headers
+    const authToken = req.headers['x-auth-token'];
+
+    // Check if authToken is empty or null
+    if (!authToken || authToken.trim() === '') {
+        res.status(401).send('Unauthorized: No auth token provided');
+        return;
+    }
+
+    if (authenticateToken(authToken)) {
+        next(); // Proceed to the proxy if authenticated
+    } else {
+        res.status(403).send('Access denied'); // Send a 403 error if not authenticated
+    }
+});
+
+// Proxy request to Kibana if authentication is successful
+app.use('/', proxy(kibanaHost));
+
+// Listen on configured port
+app.listen(serverPort, () => {
+    logger.info(`Server running at http:${serverPort}/`);
+})
