bdb8840f8a7aefd3ca236e18d16bd3e4041decee
bhanuprakash-egov <bhanu.prakash@egovernments.org>
2024-04-29 15:36:18 +0530

    removing apimodel

--- health-services/transformer/src/main/java/org/egov/transformer/models/pgr/Role.java
+++ health-services/transformer/src/main/java/org/egov/transformer/models/pgr/Role.java
@@ -1,14 +1,12 @@
 package org.egov.transformer.models.pgr;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import io.swagger.annotations.ApiModel;
 import lombok.*;
 import org.springframework.validation.annotation.Validated;
 
 /**
  * minimal representation of the Roles in the system to be carried along in UserInfo with RequestHeader meta data. Actual authorization service to extend this to have more role related attributes
  */
-@ApiModel(description = "minimal representation of the Roles in the system to be carried along in UserInfo with RequestHeader meta data. Actual authorization service to extend this to have more role related attributes ")
 @Validated
 @javax.annotation.Generated(value = "org.egov.codegen.SpringBootCodegen", date = "2020-07-15T11:35:33.568+05:30")
 
