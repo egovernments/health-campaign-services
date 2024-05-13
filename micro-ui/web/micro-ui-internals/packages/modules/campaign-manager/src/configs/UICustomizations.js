e9165661efeb04de3ec760b6a7774fe693c468eb
nabeelmd-eGov <94039229+nabeelmd-eGov@users.noreply.github.com>
2024-05-13 16:11:36 +0530

    added upcoming tab in my campaign (#555)
    * summary date format fix
    
    * product error fix, draft calls fix
    
    * added upcoming tab in my campaign
    
    ---------
    
    Co-authored-by: nabeelmd-eGov <nabeel.md@egovernments.com>

--- micro-ui/web/micro-ui-internals/packages/modules/campaign-manager/src/configs/UICustomizations.js
+++ micro-ui/web/micro-ui-internals/packages/modules/campaign-manager/src/configs/UICustomizations.js
@@ -20,9 +20,11 @@ export const UICustomizations = {
       const { campaignName, campaignType } = data?.state?.searchForm || {};
       data.body.CampaignDetails = {
         tenantId: tenantId,
-        status: ["started","In Progress"],
+        status: ["started", "In Progress"],
         createdBy: Digit.UserService.getUser().info.uuid,
-        // startDate: Date.now(),
+        campaignsIncludeDates: true,
+        startDate: Date.now(),
+        endDate: Date.now(),
         pagination: {
           sortBy: "createdTime",
           sortOrder: "desc",
@@ -111,9 +113,101 @@ export const UICustomizations = {
       const { campaignName, campaignType } = data?.state?.searchForm || {};
       data.body.CampaignDetails = {
         tenantId: tenantId,
-        status: ["started","In Progress"],
+        status: ["started", "In Progress"],
         endDate: Date.now() - 24 * 60 * 60 * 1000,
         createdBy: Digit.UserService.getUser().info.uuid,
+        pagination: {
+          sortBy: "createdTime",
+          sortOrder: "desc",
+          limit: limit,
+          offset: offset,
+        },
+      };
+      if (campaignName) {
+        data.body.CampaignDetails.campaignName = campaignName;
+      }
+      if (campaignType) {
+        data.body.CampaignDetails.projectType = campaignType?.[0]?.code;
+      }
+      delete data.body.custom;
+      delete data.body.inbox;
+      delete data.params;
+      return data;
+    },
+    populateCampaignTypeReqCriteria: () => {
+      const tenantId = Digit.ULBService.getCurrentTenantId();
+
+      return {
+        url: "/egov-mdms-service/v1/_search",
+        params: { tenantId },
+        body: {
+          MdmsCriteria: {
+            tenantId: tenantId,
+            moduleDetails: [
+              {
+                moduleName: "HCM-PROJECT-TYPES",
+                masterDetails: [
+                  {
+                    name: "projectTypes",
+                  },
+                ],
+              },
+            ],
+          },
+        },
+        changeQueryName: "setWorkflowStatus",
+        config: {
+          enabled: true,
+          select: (data) => {
+            return data?.MdmsRes?.["HCM-PROJECT-TYPES"]?.projectTypes;
+          },
+        },
+      };
+    },
+    getCustomActionLabel: (obj, row) => {
+      return "";
+    },
+    additionalCustomizations: (row, key, column, value, t, searchResult) => {
+      switch (key) {
+        case "CAMPAIGN_NAME":
+          return (
+            <span className="link">
+              <Link to={`/${window.contextPath}/employee/campaign/setup-campaign?id=${row.id}&preview=${true}&action=${false}`}>
+                {String(value ? (column.translate ? t(column.prefix ? `${column.prefix}${value}` : value) : value) : t("ES_COMMON_NA"))}
+              </Link>
+            </span>
+          );
+
+        case "CAMPAIGN_START_DATE":
+          return Digit.DateUtils.ConvertEpochToDate(value);
+        case "CAMPAIGN_END_DATE":
+          return Digit.DateUtils.ConvertEpochToDate(value);
+        default:
+          return "case_not_found";
+      }
+    },
+    onCardClick: (obj) => {
+      return `view-test-results?tenantId=${obj?.apiResponse?.businessObject?.tenantId}&id=${obj?.apiResponse?.businessObject?.testId}&from=TQM_BREAD_INBOX`;
+    },
+    onCardActionClick: (obj) => {
+      return `view-test-results?tenantId=${obj?.apiResponse?.businessObject?.tenantId}&id=${obj?.apiResponse?.businessObject?.testId}&from=TQM_BREAD_INBOX`;
+    },
+    getCustomActionLabel: (obj, row) => {
+      return "TQM_VIEW_TEST_DETAILS";
+    },
+  },
+  MyCampaignConfigUpcoming: {
+    preProcess: (data, additionalDetails) => {
+      const tenantId = Digit.ULBService.getCurrentTenantId();
+      data.body = { RequestInfo: data.body.RequestInfo };
+      const { limit, offset } = data?.state?.tableForm || {};
+      const { campaignName, campaignType } = data?.state?.searchForm || {};
+      data.body.CampaignDetails = {
+        tenantId: tenantId,
+        status: ["started", "In Progress"],
+        createdBy: Digit.UserService.getUser().info.uuid,
+        campaignsIncludeDates: false,
+        startDate: Date.now() + 24 * 60 * 60 * 1000,
         pagination: {
           sortBy: "createdTime",
           sortOrder: "desc",
