8dcbe1bdf486d11767312ed9148f2ded1134ff50
ashish-egov <137176738+ashish-egov@users.noreply.github.com>
2024-05-16 15:31:58 +0530

    Update genericUtils.ts (#599)
    * Update genericUtils.ts
    
    * Update constants.ts

--- utilities/project-factory/src/server/utils/genericUtils.ts
+++ utilities/project-factory/src/server/utils/genericUtils.ts
@@ -52,11 +52,16 @@ const throwErrorViaRequest = (message: any = "Internal Server Error") => {
   }
 };
 
+function capitalizeFirstLetter(str: string | undefined) {
+  if (!str) return str;
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
 const throwError = (module = "COMMON", status = 500, code = "UNKNOWN_ERROR", description: any = null) => {
   const errorResult: any = getErrorCodes(module, code);
   status = errorResult?.code == "UNKNOWN_ERROR" ? 500 : status;
-  let error: any = new Error(errorResult?.message);
-  error = Object.assign(error, { status, code: errorResult?.code, description });
+  let error: any = new Error(capitalizeFirstLetter(errorResult?.message));
+  error = Object.assign(error, { status, code: errorResult?.code, description: capitalizeFirstLetter(description) });
   logger.error(error);
   throw error;
 };
