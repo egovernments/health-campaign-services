e3ee0ea35e304e2c2d7cb86da1d4c21c9814f124
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2024-05-20 11:43:25 +0530

    updated the loggers

--- utilities/project-factory/src/server/Kafka/Producer.ts
+++ utilities/project-factory/src/server/Kafka/Producer.ts
@@ -1,5 +1,6 @@
 import config from '../config'; // Importing configuration settings
 import { Producer, KafkaClient } from 'kafka-node'; // Importing Producer and KafkaClient from 'kafka-node' library
+import { logger } from "../utils/logger";
 
 // Creating a new Kafka client instance using the configured Kafka broker host
 const kafkaClient = new KafkaClient({
@@ -12,13 +13,13 @@ const producer = new Producer(kafkaClient, { partitionerType: 2 }); // Using par
 
 // Event listener for 'ready' event, indicating that the producer is ready to send messages
 producer.on('ready', () => {
-    console.log('Producer is ready'); // Log message indicating producer is ready
+    logger.info('Producer is ready'); // Log message indicating producer is ready
 });
 
 // Event listener for 'error' event, indicating that the producer encountered an error
 producer.on('error', (err) => {
-    console.log('Producer is in error state'); // Log message indicating producer is in error state
-    console.log(err.stack || err); // Log the error stack or message
+    logger.error('Producer is in error state'); // Log message indicating producer is in error state
+    console.error(err.stack || err); // Log the error stack or message
 });
 
 export { producer }; // Exporting the producer instance for external use
