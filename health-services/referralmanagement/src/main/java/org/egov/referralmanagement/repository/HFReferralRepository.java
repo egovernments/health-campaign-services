e9c0367089b22fa3a79bb51db054731e05a322e8
kanishq-egov <138671649+kanishq-egov@users.noreply.github.com>
2024-06-17 17:26:20 +0530

    HLM-6196: Search failing, updated GenericQueryBuilder.java, added changes to get all … (#769)
    * HLM Health-HRMS bug fix, user was set to null in hrms update as the t… (#761)
    
    * HLM Health-HRMS bug fix, user was set to null in hrms update as the tenantid was not provided during internal search
    
    * HLM updated hrms user type
    
    * updated type from individual create
    
    * Addressed code review comments
    
    * Added code comments and fixme todo as per code review comments
    
    * HLM updated flyway migration docker version
    
    * Revert "HLM updated flyway migration docker version"
    
    This reverts commit 1f1167ea0fc5ee5ed259f85cbdf9777e31eb868f.
    
    * Updated code comments on EmployeeService update method
    
    * Update EmployeeService.java
    
    * Updated code comments on EmployeeService, added changes for NPE handling
    
    ---------
    
    Co-authored-by: kavi_elrey@1993 <25226238+kavi-egov@users.noreply.github.com>
    
    * HLM-6196: updated GenericQueryBuilder.java, added changes to get all the fields except Object class
    
    * hlm-6196: update common library reference for dev testing"
    
    * HLM-6196: updated health-service-common version to 1.0.17-SNAPSHOT and added default for included deleted
    
    * HLM-6196: updated genericrepository
    
    * HLM-6196: fixed testcases
    
    * HLM-6196: fixed individual search query error
    
    * taskresource additional fields column added
    
    * HLM-6196: updated health services models TaskResource, added AdditionalField
    
    * HLM-6196: hfreferral, referral, sideeffect - edge case fix
    
    * HLM-6196: removed todo from urlparams
    
    ---------
    
    Co-authored-by: kavi_elrey@1993 <25226238+kavi-egov@users.noreply.github.com>
    Co-authored-by: Vishal <sarasani.vishalreddy@egovernments.org>

--- health-services/referralmanagement/src/main/java/org/egov/referralmanagement/repository/HFReferralRepository.java
+++ health-services/referralmanagement/src/main/java/org/egov/referralmanagement/repository/HFReferralRepository.java
@@ -20,6 +20,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.CollectionUtils;
 import org.springframework.util.ReflectionUtils;
 
 import static org.egov.common.utils.CommonUtils.getIdMethod;
@@ -81,7 +82,11 @@ public class HFReferralRepository extends GenericRepository<HFReferral> {
         query = query.replace("clientReferenceId IN (:clientReferenceId)", "hf.clientReferenceId IN (:clientReferenceId)");
 
         // Add additional conditions based on tenant ID, includeDeleted, and lastChangedSince.
-        query = query + " and hf.tenantId=:tenantId ";
+        if(CollectionUtils.isEmpty(whereFields)) {
+            query = query + " where hf.tenantId=:tenantId ";
+        } else {
+            query = query + " and hf.tenantId=:tenantId ";
+        }
         if (Boolean.FALSE.equals(includeDeleted)) {
             query = query + "and hf.isDeleted=:isDeleted ";
         }
