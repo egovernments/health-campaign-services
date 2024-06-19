dc4148bf72ddf0f85cbe0afff58daaa49935b60d
kanishq-egov <kanishq.bhatnagar@egovernments.org>
2024-06-19 13:01:25 +0530

    HLM-6196: updated individual search as it was with exclude annotation

--- health-services/libraries/health-services-models/src/main/java/org/egov/common/models/individual/IndividualSearch.java
+++ health-services/libraries/health-services-models/src/main/java/org/egov/common/models/individual/IndividualSearch.java
@@ -50,9 +50,11 @@ public class IndividualSearch extends EgovOfflineSearchModel {
     private List<String> mobileNumber = null;
 
     @JsonProperty("socialCategory")
+    @Exclude
     private String socialCategory = null;
 
     @JsonProperty("wardCode")
+    @Exclude
     private String wardCode = null;
 
     @JsonProperty("individualName")
@@ -60,29 +62,37 @@ public class IndividualSearch extends EgovOfflineSearchModel {
     private String individualName = null;
 
     @JsonProperty("createdFrom")
+    @Exclude
     private BigDecimal createdFrom = null;
 
     @JsonProperty("createdTo")
+    @Exclude
     private BigDecimal createdTo = null;
 
     @JsonProperty("identifier")
     @Valid
+    @Exclude
     private Identifier identifier = null;
 
     @JsonProperty("boundaryCode")
+    @Exclude
     private String boundaryCode = null;
 
     @JsonProperty("roleCodes")
+    @Exclude
     private List<String> roleCodes = null;
 
     @JsonProperty("username")
+    @Exclude
     private List<String> username;
 
     @JsonProperty("userId")
+    @Exclude
     private List<Long> userId;
 
     @JsonProperty("userUuid")
     @Size(min = 1)
+    @Exclude
     private List<String> userUuid;
 
     @Exclude
