833928c25ae1ddb92c4f9d581b147e7ff4d2bc0b
siddhant-nawale-egov <162107530+siddhant-nawale-egov@users.noreply.github.com>
2024-06-19 11:54:09 +0530

    removing pl tl and template files

--- frontend/microplan-ui/web/src/Customisations/index.js
+++ frontend/microplan-ui/web/src/Customisations/index.js
@@ -1,19 +1,9 @@
-import { ptComponents } from "./pt";
-import { tlComponents } from "./tl";
-
 var Digit = window.Digit || {};
 
-const customisedComponent = {
-    ...ptComponents,
-    ...tlComponents
-}
-
-
+const customisedComponent = {};
 
 export const initCustomisationComponents = () => {
-    Object.entries(customisedComponent).forEach(([key, value]) => {
-        Digit.ComponentRegistryService.setComponent(key, value);
-    });
+  Object.entries(customisedComponent).forEach(([key, value]) => {
+    Digit.ComponentRegistryService.setComponent(key, value);
+  });
 };
-
-
