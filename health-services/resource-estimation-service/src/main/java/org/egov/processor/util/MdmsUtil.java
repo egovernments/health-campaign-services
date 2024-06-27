90dd4d2f3fd6695fb156da716862a600c6ea2c55
Nilesh Shinolikar <163095321+devdatta-egov@users.noreply.github.com>
2024-06-27 18:18:52 +0530

    Resource service read me from master (#801)
    * Read me changes from master branch
    
    * change properties file
    
    * Code review comment

--- health-services/resource-estimation-service/src/main/java/org/egov/processor/util/MdmsUtil.java
+++ health-services/resource-estimation-service/src/main/java/org/egov/processor/util/MdmsUtil.java
@@ -74,7 +74,7 @@ public class MdmsUtil {
 		if (result == null || ObjectUtils.isEmpty(result)) {
 			log.error(NO_MDMS_DATA_FOUND_FOR_GIVEN_TENANT_MESSAGE + " - " + tenantId);
 			throw new CustomException(NO_MDMS_DATA_FOUND_FOR_GIVEN_TENANT_CODE,
-					NO_MDMS_DATA_FOUND_FOR_GIVEN_TENANT_MESSAGE);
+					"no data found for the given tenantid "+tenantId + " for master name "+ServiceConstants.MDMS_MASTER_SCHEMAS);
 		}
 		return result;
 	}
@@ -140,6 +140,92 @@ public class MdmsUtil {
 		}
 
 		return properties;
+	}
+	
+	/**
+	 * Parses the provided JSON string containing master data, extracts common constants,
+	 * and returns them as a map of name-value pairs.
+	 *
+	 * @param masterDataJson JSON string representing master data
+	 * @return Map<String, Object> containing common constants where keys are constant names and values are constant values
+	 */
+	public Map<String, Object> filterMasterDataForLocale(String masterDataJson)  {
+		Map<String, Object> properties = new HashMap<>();
+		Map<String, Object> masterData = JsonUtils.parseJson(masterDataJson, Map.class);
+		Map<String, Object> planModule = (Map<String, Object>) masterData.get(ServiceConstants.MDMS_PLAN_MODULE_NAME);
+		List<Map<String, Object>> commonConstantsMap = (List<Map<String, Object>>) planModule
+				.get(ServiceConstants.MDMS_MASTER_COMMON_CONSTANTS);
+		log.info("masterDataJson ==>" + commonConstantsMap);
+		for (Map<String, Object> commonConstantMap : commonConstantsMap) {
+			properties.put((String) commonConstantMap.get("name"), (String) commonConstantMap.get("value"));
+	
+		}
+
+		return properties;
+	}
+	
+	/**
+	 * Fetches MDMS (Master Data Management System) data for common constants based on the provided request info and tenant ID.
+	 * Constructs an MDMS request, sends a POST request to the configured MDMS endpoint, and processes the response.
+	 *
+	 * @param requestInfo The request information containing context like user details and timestamp.
+	 * @param tenantId    The ID of the tenant for which MDMS data is requested.
+	 * @return A filtered map of MDMS data for common constants, specific to the locale.
+	 * @throws CustomException       If no MDMS data is found for the given tenant ID.
+	 * @throws JsonMappingException  If there's an issue mapping JSON response to Java objects.
+	 * @throws JsonProcessingException If there's an issue processing JSON during conversion.
+	 */
+	public  Map<String, Object> fetchMdmsDataForCommonConstants(RequestInfo requestInfo, String tenantId) {
+		StringBuilder uri = new StringBuilder();
+		uri.append(configs.getMdmsHost()).append(configs.getMdmsEndPoint());
+		MdmsCriteriaReq mdmsCriteriaReq = getMdmsRequestForCommonConstants(requestInfo, tenantId);
+		Object response = new HashMap<>();
+		MdmsResponse mdmsResponse = new MdmsResponse();
+		try {
+			response = restTemplate.postForObject(uri.toString(), mdmsCriteriaReq, Map.class);
+			mdmsResponse = mapper.convertValue(response, MdmsResponse.class);
+		} catch (Exception e) {
+			log.error(ERROR_WHILE_FETCHING_FROM_MDMS, e);
+		}
+
+		Object result = mdmsResponse.getMdmsRes();
+		if (result == null || ObjectUtils.isEmpty(result)) {
+			log.error(NO_MDMS_DATA_FOUND_FOR_GIVEN_TENANT_MESSAGE + " - " + tenantId);
+			throw new CustomException(NO_MDMS_DATA_FOUND_FOR_GIVEN_TENANT_CODE,
+					"no data found for the given tenantid "+tenantId + " for master name "+ServiceConstants.MDMS_MASTER_COMMON_CONSTANTS);
+		}
+		return filterMasterDataForLocale(result.toString());
+	}
+	
+	/**
+	 * Constructs an MDMS (Master Data Management System) request object for fetching common constants.
+	 *
+	 * @param requestInfo The request information containing context like user details and timestamp.
+	 * @param tenantId    The ID of the tenant for which MDMS data is requested.
+	 * @return MdmsCriteriaReq object encapsulating the MDMS criteria for fetching common constants.
+	 */
+	public MdmsCriteriaReq getMdmsRequestForCommonConstants(RequestInfo requestInfo, String tenantId) {
+
+		ModuleDetail moduleDetail = getPlanModulesCommonConstants();
+		List<ModuleDetail> moduleDetails = new LinkedList<>();
+		moduleDetails.add(moduleDetail);
+		MdmsCriteria mdmsCriteria = MdmsCriteria.builder().moduleDetails(moduleDetails).tenantId(tenantId).build();
+		return MdmsCriteriaReq.builder().mdmsCriteria(mdmsCriteria).requestInfo(requestInfo).build();
+	}
+	
+	
+	/**
+	 * Constructs a ModuleDetail object for fetching common constants from MDMS.
+	 *
+	 * @return ModuleDetail object representing the module configuration for common constants.
+	 */
+	private ModuleDetail getPlanModulesCommonConstants() {
+		List<MasterDetail> assumptionMasterDetails = new ArrayList<>();
+		MasterDetail schemaDetails = MasterDetail.builder().name(ServiceConstants.MDMS_MASTER_COMMON_CONSTANTS).build();
+		assumptionMasterDetails.add(schemaDetails);
+
+		return ModuleDetail.builder().masterDetails(assumptionMasterDetails)
+				.moduleName(ServiceConstants.MDMS_PLAN_MODULE_NAME).build();
 	}
 
 }
