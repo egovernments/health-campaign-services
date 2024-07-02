2b3fbdbbdb4d7c2fad0461a569288ea4e2043465
SivajiGanesh Nangireddy <sivaji.ganesh@egovernments.org>
2024-07-02 12:22:17 +0530

    HLM-4989: refactor - remove unused imports

--- health-services/libraries/health-services-models/src/main/java/org/egov/common/models/facility/Field.java
+++ health-services/libraries/health-services-models/src/main/java/org/egov/common/models/facility/Field.java
@@ -1,7 +1,5 @@
 package org.egov.common.models.facility;
 
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
