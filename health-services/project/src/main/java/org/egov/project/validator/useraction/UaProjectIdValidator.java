0fc7abff62f80ecbfa252fa728ea002abdf1b97f
kanishq-egov <kanishq.bhatnagar@egovernments.org>
2024-08-12 13:50:44 +0530

    HLM updated UaProjectIdValidator documentation

--- health-services/project/src/main/java/org/egov/project/validator/useraction/UaProjectIdValidator.java
+++ health-services/project/src/main/java/org/egov/project/validator/useraction/UaProjectIdValidator.java
@@ -26,16 +26,22 @@ import static org.egov.common.utils.CommonUtils.populateErrorDetails;
 import static org.egov.common.utils.ValidatorUtils.getErrorForNonExistentRelatedEntity;
 
 /**
- * UaProjectIdValidator is responsible for validating the Project IDs in UserActionBulkRequest.
- * It checks if the Project IDs present in the UserAction entities exist in the Project repository.
+ * UaProjectIdValidator is responsible for validating Project IDs within UserAction entities contained
+ * in a UserActionBulkRequest. This validator ensures that the Project IDs in UserAction entities exist
+ * in the Project repository.
  */
 @Component
 @Order(value = 6)
 @Slf4j
 public class UaProjectIdValidator implements Validator<UserActionBulkRequest, UserAction> {
 
-    private ProjectRepository projectRepository;
+    private final ProjectRepository projectRepository;
 
+    /**
+     * Constructs an UaProjectIdValidator with the specified ProjectRepository.
+     *
+     * @param projectRepository the repository used to validate Project IDs.
+     */
     @Autowired
     public UaProjectIdValidator(ProjectRepository projectRepository) {
         this.projectRepository = projectRepository;
@@ -46,41 +52,50 @@ public class UaProjectIdValidator implements Validator<UserActionBulkRequest, Us
      * It checks if the Project IDs present in the UserAction entities exist in the Project repository.
      *
      * @param request the UserActionBulkRequest containing UserAction entities to be validated.
-     * @return a map of UserAction entities to a list of Errors encountered during validation.
+     * @return a map where keys are UserAction entities and values are lists of Errors encountered during validation.
      */
     @Override
     public Map<UserAction, List<Error>> validate(UserActionBulkRequest request) {
         log.info("Starting validation of project IDs in UserActionBulkRequest with {} entities", request.getUserActions().size());
 
+        // Create a map to hold error details for each UserAction entity
         Map<UserAction, List<Error>> errorDetailsMap = new HashMap<>();
         List<UserAction> entities = request.getUserActions();
 
-        // Retrieve the class of the UserAction entities
+        // Retrieve the class type of the UserAction entities
         Class<?> objClass = getObjClass(entities);
         log.debug("Retrieved UserAction entity class: {}", objClass.getName());
 
-        // Retrieve the method to get ProjectId from UserAction entities
+        // Retrieve the method that gets the Project ID from a UserAction entity
         Method idMethod = getMethod("getProjectId", objClass);
         log.debug("Retrieved getProjectId method from UserAction entity class");
 
         // Create a map of Project IDs to UserAction entities
-        Map<String, UserAction> eMap = getIdToObjMap(entities.stream().filter(notHavingErrors()).collect(Collectors.toList()), idMethod);
+        Map<String, UserAction> eMap = getIdToObjMap(
+                entities.stream()
+                        .filter(notHavingErrors()) // Filter out entities that already have errors
+                        .collect(Collectors.toList()), // Collect to a list
+                idMethod
+        );
         log.info("Created map of Project IDs to UserAction entities with {} entries", eMap.size());
 
+        // Proceed if there are Project IDs to validate
         if (!eMap.isEmpty()) {
-            List<String> entityIds = new ArrayList<>(eMap.keySet());
+            List<String> entityIds = new ArrayList<>(eMap.keySet()); // List of Project IDs
             log.debug("List of Project IDs to validate: {}", entityIds);
 
             // Validate the Project IDs by checking their existence in the Project repository
             List<String> existingProjectIds = projectRepository.validateIds(entityIds, getIdFieldName(idMethod));
             log.info("Retrieved list of existing Project IDs from Project repository: {}", existingProjectIds);
 
-            // Identify invalid UserAction entities with non-existent Project IDs
-            List<UserAction> invalidEntities = entities.stream().filter(notHavingErrors()).filter(entity ->
-                    !existingProjectIds.contains(entity.getProjectId())).collect(Collectors.toList());
+            // Identify UserAction entities with non-existent Project IDs
+            List<UserAction> invalidEntities = entities.stream()
+                    .filter(notHavingErrors()) // Filter out entities that already have errors
+                    .filter(entity -> !existingProjectIds.contains(entity.getProjectId())) // Find entities with invalid Project IDs
+                    .collect(Collectors.toList());
             log.info("Identified {} invalid UserAction entities with non-existent Project IDs", invalidEntities.size());
 
-            // Populate error details for invalid UserAction entities
+            // Populate error details for UserAction entities with invalid Project IDs
             invalidEntities.forEach(userAction -> {
                 Error error = getErrorForNonExistentRelatedEntity(userAction.getProjectId());
                 populateErrorDetails(userAction, error, errorDetailsMap);
