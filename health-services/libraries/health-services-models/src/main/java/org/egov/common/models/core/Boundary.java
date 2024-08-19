2705fed0751062ff698673fd71f9f63e0ba52a04
kanishq-egov <138671649+kanishq-egov@users.noreply.github.com>
2024-08-19 15:48:53 +0530

    Hcmpre 309 rebase (#857)
    * removed digit-models from health-services-models library
    
    * removed digit-models from health-services-models library
    
    * added SMSRequest model
    
    * Updated individual models
    
    * Updating health services models version in health services common library
    
    * version bump
    
    * hcmpre-309: merged removed-digit-models and rebase to dev
    
    * HLM-309: updated common models version
    
    ---------
    
    Co-authored-by: shubhang-egov <shubhang.dhanesha@egovernments.org>

--- health-services/libraries/health-services-models/src/main/java/org/egov/common/models/core/Boundary.java
+++ health-services/libraries/health-services-models/src/main/java/org/egov/common/models/core/Boundary.java
@@ -1,10 +1,9 @@
 package org.egov.common.models.core;
 
-import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotNull;
-
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.JsonNode;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
