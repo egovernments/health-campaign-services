448845808c6614e08069644284abe79a70d41dac
nitish-egov <nitish@egovernments.org>
2024-08-27 16:59:03 +0530

    updated format and id name  for user name

--- health-services/project-factory/src/server/config/index.ts
+++ health-services/project-factory/src/server/config/index.ts
@@ -156,8 +156,8 @@ const config = {
     idgen: {
       format: process.env.CMP_IDGEN_FORMAT || "CMP-[cy:yyyy-MM-dd]-[SEQ_EG_CMP_ID]",
       idName: process.env.CMP_IDGEN_IDNAME || "campaign.number",
-      idNameForUserNameGeneration: "user.name",
-      formatForUserName: "EMP-[SEQ_EG_CMP_ID]"
+      idNameForUserNameGeneration: "username.name",
+      formatForUserName: "USR-[SEQ_EG_CMP_ID]"
     },
     matchFacilityData: false,
     retryCount: process.env.CREATE_RESOURCE_RETRY_COUNT || "3",
