0f7f6bd59e560e97f7b6b5005fb3534b545335b6
kanishq-egov <kanishq.bhatnagar@egovernments.org>
2024-08-28 10:09:54 +0530

    HCMPRE-203: Updated with Primary annotation for rest template bean

--- health-services/transformer/src/main/java/org/egov/transformer/aggregator/config/RestTemplateConfig.java
+++ health-services/transformer/src/main/java/org/egov/transformer/aggregator/config/RestTemplateConfig.java
@@ -10,6 +10,7 @@ import javax.net.ssl.X509TrustManager;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -48,6 +49,7 @@ public class RestTemplateConfig {
 
 
   @Bean("customRestTemplate")
+  @Primary
   public RestTemplate restTemplate() {
     trustSelfSignedSSL();
     return new RestTemplate();
