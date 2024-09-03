60cd13e5474d3fd25d6cb1c561929be215a2ad0c
prerna-egov <prerna.kumari@egovernments.org>
2024-09-03 18:03:16 +0530

    added logic to convert some values to integer

--- health-services/transformer/src/main/java/org/egov/transformer/service/HouseholdService.java
+++ health-services/transformer/src/main/java/org/egov/transformer/service/HouseholdService.java
@@ -1,5 +1,6 @@
 package org.egov.transformer.service;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.exception.ExceptionUtils;
@@ -10,14 +11,16 @@ import org.egov.transformer.config.TransformerProperties;
 import org.egov.transformer.http.client.ServiceRequestClient;
 import org.springframework.stereotype.Component;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+
+import static org.egov.transformer.Constants.*;
 
 @Component
 @Slf4j
 public class HouseholdService {
     private final TransformerProperties transformerProperties;
     private final ServiceRequestClient serviceRequestClient;
+    private static final Set<String> ADDITIONAL_DETAILS_INTEGER_FIELDS = new HashSet<>(Arrays.asList(PREGNANTWOMEN, CHILDREN, NO_OF_ROOMS));
 
     public HouseholdService(TransformerProperties transformerProperties, ServiceRequestClient serviceRequestClient) {
         this.transformerProperties = transformerProperties;
@@ -53,20 +56,33 @@ public class HouseholdService {
     }
 
     public void additionalFieldsToDetails(ObjectNode additionalDetails, Object additionalFields) {
-        if (additionalFields instanceof List<?>) {
-            List<?> fieldsList = (List<?>) additionalFields;
+        if (!(additionalFields instanceof List<?>)) {
+            throw new IllegalArgumentException("additionalFields is not of the expected type List<Field>");
+        }
 
-            for (Object item : fieldsList) {
-                if (item instanceof Field) {
-                    Field field = (Field) item;
-                    // Check if the key already exists in additionalDetails
-                    if (!additionalDetails.has(field.getKey())) {
-                        additionalDetails.put(field.getKey(), field.getValue());
+        for (Object item : (List<?>) additionalFields) {
+            if (item instanceof Field) {
+                Field field = (Field) item;
+                String key = field.getKey();
+                String value = field.getValue();
+
+                if (!additionalDetails.has(key)) {
+                    if (ADDITIONAL_DETAILS_INTEGER_FIELDS.contains(key)) {
+                        additionalDetails.put(key, parseInteger(value, key));
+                    } else {
+                        additionalDetails.put(key, value);
                     }
                 }
             }
-        } else {
-            throw new IllegalArgumentException("additionalFields is not of the expected type List<Field>");
+        }
+    }
+
+    private int parseInteger(String value, String key) {
+        try {
+            return Integer.parseInt(value);
+        } catch (NumberFormatException e) {
+            log.warn("Invalid number format for key '{}': value '{}'. Storing as 0.", key, value);
+            return 0;
         }
     }
 }
