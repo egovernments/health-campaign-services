f99475a8003bf8835dc1c3d4806998ffa272936e
Priyanka-eGov <74049060+Priyanka-eGov@users.noreply.github.com>
2024-09-05 17:26:57 +0530

    HCMPRE-169 code review comments (#835)
    * HCMPRE-169 code review comments
    
    * HCMPRE-169 adding code comments
    
    * HCMPRE-169 adding code comments
    
    * HCMPRE-169 changing mdms v2 field name for uom
    
    * HCMPRE-169 code review comments
    
    * HCMPRE-169 code review comments

--- health-services/plan-service/src/main/java/digit/service/enrichment/EnrichmentService.java
+++ health-services/plan-service/src/main/java/digit/service/enrichment/EnrichmentService.java
@@ -6,14 +6,13 @@ import digit.web.models.PlanConfiguration;
 import digit.web.models.PlanConfigurationRequest;
 import digit.web.models.ResourceMapping;
 import lombok.extern.slf4j.Slf4j;
-import org.egov.common.utils.AuditDetailsEnrichmentUtil;
 import org.egov.common.utils.UUIDEnrichmentUtil;
 import org.egov.tracer.model.CustomException;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
-import static digit.config.ServiceConstants.USERINFO_MISSING_CODE;
-import static digit.config.ServiceConstants.USERINFO_MISSING_MESSAGE;
+import static org.egov.common.utils.AuditDetailsEnrichmentUtil.prepareAuditDetails;
+
 import org.springframework.util.ObjectUtils;
 
 @Component
@@ -27,23 +26,13 @@ public class EnrichmentService {
 
     /**
      * Enriches the PlanConfigurationRequest for creating a new plan configuration.
-     * This method enriches the plan configuration with generated IDs, validates user information, and enriches audit details for create operation.
+     * Enriches the given plan configuration with generated IDs for plan, files, assumptions, operations, and resource mappings,
+     * validates user information, and enriches audit details for create operation.
      * @param request The PlanConfigurationRequest to be enriched.
      * @throws CustomException if user information is missing in the request.
      */
     public void enrichCreate(PlanConfigurationRequest request) {
-        enrichPlanConfiguration(request.getPlanConfiguration());
-        if(ObjectUtils.isEmpty(request.getRequestInfo().getUserInfo()))
-            throw new CustomException(USERINFO_MISSING_CODE, USERINFO_MISSING_MESSAGE);
-
-        enrichAuditDetails(request, Boolean.TRUE);
-    }
-
-    /**
-     * Enriches the given plan configuration with generated IDs for plan, files, assumptions, operations, and resource mappings.
-     * @param planConfiguration The PlanConfiguration to be enriched.
-     */
-    public void enrichPlanConfiguration(PlanConfiguration planConfiguration) {
+        PlanConfiguration planConfiguration = request.getPlanConfiguration();
         log.info("Enriching plan config with generated IDs");
 
         // Generate id for plan configuration
@@ -64,17 +53,8 @@ public class EnrichmentService {
 
         // Generate id for resource mappings
         planConfiguration.getResourceMapping().forEach(resourceMapping -> UUIDEnrichmentUtil.enrichRandomUuid(resourceMapping, "id"));
-    }
 
-    /**
-     * Enriches the audit details for the PlanConfigurationRequest based on the operation type (create or update).
-     * @param request The PlanConfigurationRequest for which audit details are to be enriched.
-     * @param isCreate A boolean indicating whether the operation is a create or update operation.
-     */
-    public void enrichAuditDetails(PlanConfigurationRequest request, Boolean isCreate) {
-        PlanConfiguration planConfiguration = request.getPlanConfiguration();
-        planConfiguration.setAuditDetails(AuditDetailsEnrichmentUtil
-                .prepareAuditDetails(planConfiguration.getAuditDetails(), request.getRequestInfo(), isCreate));
+        planConfiguration.setAuditDetails(prepareAuditDetails(planConfiguration.getAuditDetails(), request.getRequestInfo(), Boolean.TRUE));
     }
 
     /**
@@ -84,21 +64,9 @@ public class EnrichmentService {
      * @throws CustomException if user information is missing in the request.
      */
     public void enrichUpdate(PlanConfigurationRequest request) {
-        enrichPlanConfigurationForUpdate(request);
-        if (request.getRequestInfo().getUserInfo() == null)
-            throw new CustomException(USERINFO_MISSING_CODE, USERINFO_MISSING_MESSAGE);
-
-        enrichAuditDetails(request, Boolean.FALSE);
-    }
-
-    /**
-     * Enriches the plan configuration for update by generating IDs for files, assumptions, operations, and resource mappings if they are empty.
-     * @param request The PlanConfigurationRequest to be enriched for update operation.
-     */
-    public void enrichPlanConfigurationForUpdate(PlanConfigurationRequest request) {
         PlanConfiguration planConfiguration = request.getPlanConfiguration();
 
-        // For Files
+        // Generate id for Files
         planConfiguration.getFiles().forEach(file -> {
             if (ObjectUtils.isEmpty(file.getId())) {
                 UUIDEnrichmentUtil.enrichRandomUuid(file, "id");
@@ -106,27 +74,28 @@ public class EnrichmentService {
             enrichActiveForResourceMapping(file, request.getPlanConfiguration().getResourceMapping());
         });
 
-        // For Assumptions
+        // Generate id for Assumptions
         planConfiguration.getAssumptions().forEach(assumption -> {
             if (ObjectUtils.isEmpty(assumption.getId())) {
                 UUIDEnrichmentUtil.enrichRandomUuid(assumption, "id");
             }
         });
 
-        // For Operations
+        // Generate id for Operations
         planConfiguration.getOperations().forEach(operation -> {
             if (ObjectUtils.isEmpty(operation.getId())) {
                 UUIDEnrichmentUtil.enrichRandomUuid(operation, "id");
             }
         });
 
-        // For ResourceMappings
+        // Generate id for ResourceMappings
         planConfiguration.getResourceMapping().forEach(resourceMapping -> {
             if (ObjectUtils.isEmpty(resourceMapping.getId())) {
                 UUIDEnrichmentUtil.enrichRandomUuid(resourceMapping, "id");
             }
         });
 
+        planConfiguration.setAuditDetails(prepareAuditDetails(request.getPlanConfiguration().getAuditDetails(), request.getRequestInfo(), Boolean.FALSE));
     }
 
     /**
@@ -138,11 +107,9 @@ public class EnrichmentService {
     public void enrichActiveForResourceMapping(File file, List<ResourceMapping> resourceMappings) {
         if (!file.getActive()) {
             // Set all corresponding resource mappings to inactive
-            for (ResourceMapping mapping : resourceMappings) {
-                if (mapping.getFilestoreId().equals(file.getFilestoreId())) {
-                    mapping.setActive(false);
-                }
-            }
+            resourceMappings.stream()
+                    .filter(mapping -> mapping.getFilestoreId().equals(file.getFilestoreId()))
+                    .forEach(mapping -> mapping.setActive(false));
         }
     }
 
