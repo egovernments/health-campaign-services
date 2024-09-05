8fe6d1aebadca09bc7e723df5db178fb67e47849
bhanu <bhanu.prakash@egovernments.org>
2024-09-05 14:24:30 +0530

    adding RECEIVED and DISPATCHED in validator

--- health-services/stock/src/main/java/org/egov/stock/service/FacilityService.java
+++ health-services/stock/src/main/java/org/egov/stock/service/FacilityService.java
@@ -15,6 +15,7 @@ import org.egov.common.models.project.ProjectFacilitySearchRequest;
 import org.egov.common.models.stock.SenderReceiverType;
 import org.egov.common.models.stock.Stock;
 import org.egov.common.models.stock.StockReconciliation;
+import org.egov.common.models.stock.TransactionType;
 import org.egov.stock.config.StockConfiguration;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
@@ -94,10 +95,10 @@ public class FacilityService {
 
 				Stock stock = (Stock) entity;
 
-				if (SenderReceiverType.WAREHOUSE.equals(stock.getSenderType())) {
+				if (SenderReceiverType.WAREHOUSE.equals(stock.getSenderType()) && TransactionType.DISPATCHED.equals(stock.getTransactionType())) {
 					facilityIds.add(stock.getSenderId());
 				}
-				if (SenderReceiverType.WAREHOUSE.equals(stock.getReceiverType())) {
+				if (SenderReceiverType.WAREHOUSE.equals(stock.getReceiverType()) && TransactionType.RECEIVED.equals(stock.getTransactionType())) {
 					facilityIds.add(stock.getReceiverId());
 				}
 			}
