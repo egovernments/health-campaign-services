b6e940f298aa71081aef8db899b2758bde64c4ab
Priyanka-eGov <priyanka@egovernments.org>
2024-09-12 17:15:16 +0530

    HCMPRE-503, HCMPRE-513 renaming executionPlanId to campaignId and adding showOnEstimationDashboard to operations.

--- health-services/plan-service/src/main/resources/db/migration/main/V20241109150000__rename_execution_id_campaign_id_ddl.sql
+++ health-services/plan-service/src/main/resources/db/migration/main/V20241109150000__rename_execution_id_campaign_id_ddl.sql
@@ -5,4 +5,4 @@ ALTER TABLE plan
 RENAME COLUMN execution_plan_id TO campaign_id;
 
 ALTER TABLE plan_configuration_operations ADD show_on_estimation_dashboard boolean;
-UPDATE plan_configuration_operations SET show_on_estimation_dashboard = true WHERE active IS NULL;
+UPDATE plan_configuration_operations SET show_on_estimation_dashboard = true WHERE show_on_estimation_dashboard IS NULL;
