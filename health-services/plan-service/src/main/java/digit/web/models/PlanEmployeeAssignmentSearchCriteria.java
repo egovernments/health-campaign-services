3f08690ff8e4e39452cba46ddfa91aea59c42f86
Tanishi Goyal <tanishi.goyal@egovernments.org>
2024-09-13 13:25:19 +0530

    added pojos for PlanEmployeeAssignment

---
+++ health-services/plan-service/src/main/java/digit/web/models/PlanEmployeeAssignmentSearchCriteria.java
@@ -0,0 +1,43 @@
+package digit.web.models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.List;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import org.springframework.validation.annotation.Validated;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Data;
+import lombok.Builder;
+
+/**
+ * PlanEmployeeAssignmentSearchCriteria
+ */
+@Validated
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class PlanEmployeeAssignmentSearchCriteria {
+
+    @JsonProperty("tenantId")
+    @Size(min = 2, max = 100)
+    @NotNull
+    private String tenantId = null;
+
+    @JsonProperty("planConfigurationId")
+    @Size(max = 64)
+    private String planConfigurationId = null;
+
+    @JsonProperty("role")
+    @Size(min = 2, max = 64)
+    private String role = null;
+
+    @JsonProperty("jurisdiction")
+    @Valid
+    private List<String> jurisdiction = null;
+
+    @JsonProperty("active")
+    private Boolean active = null;
+}
