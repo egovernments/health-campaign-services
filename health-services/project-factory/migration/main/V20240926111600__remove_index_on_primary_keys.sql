5cce04c7b94565bcf450084444de1b0b384c8a43
nitish-egov <nitish@egovernments.org>
2024-09-26 11:18:52 +0530

    removed indexes on primary keys

---
+++ health-services/project-factory/migration/main/V20240926111600__remove_index_on_primary_keys.sql
@@ -0,0 +1,5 @@
+DROP INDEX IF EXISTS idx_eg_cm_campaign_details_id;
+DROP INDEX IF EXISTS idx_eg_cm_campaign_process_id;
+DROP INDEX IF EXISTS idx_eg_cm_generated_resource_details_id;
+DROP INDEX IF EXISTS idx_eg_cm_resource_activity_id;
+DROP INDEX IF EXISTS idx_eg_cm_resource_details_id;
