63083173ee15290f16dd7d38eaab02882a468187
ansh-egov <ansh.goyal@egovernments.org>
2024-10-15 17:25:31 +0530

    added localization fix

--- health-services/project-factory/src/server/config/models/createRequestSchema.ts
+++ health-services/project-factory/src/server/config/models/createRequestSchema.ts
@@ -4,7 +4,7 @@ export const createRequestSchema = {
     "properties": {
         "type": {
             "type": "string",
-            "enum": ["boundary", "facility", "user", "boundaryWithTarget", "facilityMicroplan","boundaryManagement"]
+            "enum": ["boundary", "facility", "user", "boundaryWithTarget", "facilityMicroplan","boundaryManagement", "boundaryGeometryManagement"]
         },
         "tenantId": {
             "type": "string",
