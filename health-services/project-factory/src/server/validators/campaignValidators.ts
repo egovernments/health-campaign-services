ccf5c7dc502d809d5bca09e94be3e18881166c88
nitish-egov <137176807+nitish-egov@users.noreply.github.com>
2024-10-17 11:24:55 +0530

    some  correction of error after changes from microplan code merge (#988)
    * some  correction of error after changes from microplan code merge
    
    * added question mark

--- health-services/project-factory/src/server/validators/campaignValidators.ts
+++ health-services/project-factory/src/server/validators/campaignValidators.ts
@@ -84,7 +84,7 @@ async function fetchBoundariesFromCampaignDetails(request: any) {
     return responseBoundaries;
 }
 
-function validateTargetForNormalCampaigns(data: any, errors: any, localizedTargetColumnNames: any, config: any, localizationMap?: { [key: string]: string }) {
+function validateTargetForNormalCampaigns(data: any, errors: any, localizedTargetColumnNames: any, localizationMap?: { [key: string]: string }) {
     for (const key in data) {
         if (key !== getLocalizedName(getBoundaryTabName(), localizationMap) && key !== getLocalizedName(config?.values?.readMeTab, localizationMap)) {
             if (Array.isArray(data[key])) {
