8bfc96251c41d66efe011624d8b02fcbbdb2d3ba
Priyanka-eGov <priyanka@egovernments.org>
2024-10-17 17:17:07 +0530

    HCMPRE-599 updating Plan service models

--- health-services/resource-estimation-service/src/main/java/org/egov/processor/web/models/Resource.java
+++ health-services/resource-estimation-service/src/main/java/org/egov/processor/web/models/Resource.java
@@ -3,12 +3,13 @@ package org.egov.processor.web.models;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import java.math.BigDecimal;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.validation.annotation.Validated;
+
+import java.math.BigDecimal;
 
 /**
  * Resource
