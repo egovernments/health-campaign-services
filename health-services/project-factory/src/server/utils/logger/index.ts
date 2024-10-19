c7384b86452bd6964d92583f8f2751a10073e25d
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2024-10-19 15:59:29 +0530

    enriched the logs with file info

--- health-services/project-factory/src/server/utils/logger/index.ts
+++ health-services/project-factory/src/server/utils/logger/index.ts
@@ -1,9 +1,31 @@
-import { createLogger, format, transports } from "winston"; // Importing necessary modules from Winston library
+import { createLogger, format,  transports } from "winston"; // Importing necessary modules from Winston library
 import config from "../../config";
+const path = require('path');
+
+// Helper function to get the calling file name and line number
+const getCallerFile = (count = 4) => {
+  const originalFunc = Error.prepareStackTrace;
+  Error.prepareStackTrace = (_, stack) => stack;
+  const stack:any = new Error().stack;
+  Error.prepareStackTrace = originalFunc;
+
+  // Filter the stack to remove unwanted paths
+  const filteredStack = stack.filter((callSite:any) => {
+      const filePath = callSite.getFileName();
+      return !filePath?.includes('node_modules') && !filePath?.includes('/utils/logger/') &&  !filePath?.includes('.js')    ;
+  })
+
+  // Get the last 'count' valid callers from the filtered stack
+  const callers = filteredStack.slice(-count).map((callSite:any) => callSite.getFileName()&&({
+      file: path.basename(callSite.getFileName()),
+      line: callSite.getLineNumber(),
+  }));
+  return callers?.map((e:any)=>`${e?.file?.replace(".ts","")}:${e?.line}`);
+};
 
 // Custom log format for Winston logger
 const myFormat = format.printf(({ level, message, label, timestamp }) => {
-  return `${timestamp} [${label}] [${level}]: ${message}`; // Custom log message format
+  return `${timestamp} [${label}] ${level}: [${getCallerFile(3)}] ${message}`;
 });
 
 // Creating a logger instance with specified format and transports
