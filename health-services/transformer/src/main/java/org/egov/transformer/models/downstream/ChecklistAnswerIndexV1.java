2035dd04507aca78be27291ecaa9dc2ab2c76d88
prerna-egov <prerna.kumari@egovernments.org>
2024-10-22 17:04:05 +0530

    added POJO for checklist answers index

---
+++ health-services/transformer/src/main/java/org/egov/transformer/models/downstream/ChecklistAnswerIndexV1.java
@@ -0,0 +1,80 @@
+package org.egov.transformer.models.downstream;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Map;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChecklistAnswerIndexV1 {
+
+    @JsonProperty("id")
+    private String id;
+
+    @JsonProperty("createdTime")
+    private Long createdTime;
+
+    @JsonProperty("createdBy")
+    private String createdBy;
+
+    @JsonProperty("supervisorLevel")
+    private String supervisorLevel;
+
+    @JsonProperty("checklistName")
+    private String checklistName;
+
+    @JsonProperty("projectId")
+    private String projectId;
+
+    @JsonProperty("serviceDefinitionId")
+    private String serviceDefinitionId;
+
+    @JsonProperty("userName")
+    private String userName;
+
+    @JsonProperty("nameOfUser")
+    private String nameOfUser;
+
+    @JsonProperty("role")
+    private String role;
+
+    @JsonProperty("userAddress")
+    private String userAddress;
+
+    @JsonProperty("boundaryHierarchy")
+    private Map<String, String> boundaryHierarchy;
+
+    @JsonProperty("boundaryHierarchyCode")
+    private Map<String, String> boundaryHierarchyCode;
+
+    @JsonProperty("tenantId")
+    private String tenantId;
+
+    @JsonProperty("userId")
+    private String userId;
+
+    @JsonProperty("clientReferenceId")
+    private String clientReferenceId;
+
+    @JsonProperty("syncedTimeStamp")
+    private String syncedTimeStamp;
+
+    @JsonProperty("syncedTime")
+    private Long syncedTime;
+
+    @JsonProperty("taskDates")
+    private String taskDates;
+
+    @JsonProperty("additionalDetails")
+    private ObjectNode additionalDetails;
+
+    @JsonProperty("checklistAnswers")
+    private ObjectNode checklistAnswers;
+}
