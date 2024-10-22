73547b7ea2cff2a83898ea71d65da4ef554d7ad3
nitish-egov <nitish@egovernments.org>
2024-10-22 18:45:15 +0530

    changed the variable name to boundaryProjectMappingForProjectResourceCreation from newBoundaryProjectMapping

--- health-services/project-factory/src/server/utils/campaignMappingUtils.ts
+++ health-services/project-factory/src/server/utils/campaignMappingUtils.ts
@@ -264,8 +264,8 @@ async function getProjectMappingBody(messageObject: any, boundaryWithProject: an
     for (const key of Object.keys(boundaryWithProject)) {
         if (boundaryWithProject[key]) {
             const resources: any[] = [];
-            if (messageObject?.Campaign?.newBoundaryProjectMapping?.hasOwnProperty(key)) {
-                if (messageObject.Campaign.newBoundaryProjectMapping[key]?.projectId) {
+            if (messageObject?.Campaign?.boundaryProjectMappingForProjectResourceCreation?.hasOwnProperty(key)) {
+                if (messageObject.Campaign.boundaryProjectMappingForProjectResourceCreation[key]?.projectId) {
                     const pvarIds = getPvarIds(messageObject);
                     if (pvarIds && Array.isArray(pvarIds) && pvarIds.length > 0) {
                         resources.push({
