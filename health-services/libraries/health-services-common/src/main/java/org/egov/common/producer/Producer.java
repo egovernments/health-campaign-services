8725ddbb585d12cda23d1390a7fb56b742b96f87
saiprakash-egov <sai.prakash@egovernments.org>
2024-11-15 15:06:13 +0530

    removing kafka dependency in producer bean

--- health-services/libraries/health-services-common/src/main/java/org/egov/common/producer/Producer.java
+++ health-services/libraries/health-services-common/src/main/java/org/egov/common/producer/Producer.java
@@ -2,25 +2,26 @@ package org.egov.common.producer;
 
 import lombok.extern.slf4j.Slf4j;
 import org.egov.tracer.kafka.CustomKafkaTemplate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Service;
 
 // NOTE: If tracer is disabled change CustomKafkaTemplate to KafkaTemplate in autowiring
 
 @Service
 @Slf4j
-@ConditionalOnProperty(name = "producer.bean.enabled", havingValue = "true", matchIfMissing = false)
 public class Producer {
+    private static final Logger log = LoggerFactory.getLogger(Producer.class);
 
-    private final CustomKafkaTemplate<String, Object> kafkaTemplate;
-
-    @Autowired
-    public Producer(CustomKafkaTemplate<String, Object> kafkaTemplate) {
-        this.kafkaTemplate = kafkaTemplate;
-    }
+//    private final CustomKafkaTemplate<String, Object> kafkaTemplate;
+//
+//    @Autowired
+//    public Producer(CustomKafkaTemplate<String, Object> kafkaTemplate) {
+//        this.kafkaTemplate = kafkaTemplate;
+//    }
 
     public void push(String topic, Object value) {
-        kafkaTemplate.send(topic, value);
+        log.info(topic, value);
     }
 }
