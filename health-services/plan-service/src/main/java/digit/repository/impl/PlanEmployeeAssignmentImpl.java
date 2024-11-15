702d825c56094e6e1e597623bc49b4c9fbc842c1
Tanishi Goyal <tanishi.goyal@egovernments.org>
2024-11-15 11:57:25 +0530

    pull from microplanning-dev

--- health-services/plan-service/src/main/java/digit/repository/impl/PlanEmployeeAssignmentImpl.java
+++ health-services/plan-service/src/main/java/digit/repository/impl/PlanEmployeeAssignmentImpl.java
@@ -57,7 +57,7 @@ public class PlanEmployeeAssignmentImpl implements PlanEmployeeAssignmentReposit
         List<Object> preparedStmtList = new ArrayList<>();
         String searchQuery = queryBuilder.getPlanEmployeeAssignmentQuery(searchCriteria, preparedStmtList);
         List<PlanEmployeeAssignment> planEmployeeAssignments = jdbcTemplate.query(searchQuery, rowMapper, preparedStmtList.toArray());
-        log.info(searchQuery);
+
         return planEmployeeAssignments;
     }
 
