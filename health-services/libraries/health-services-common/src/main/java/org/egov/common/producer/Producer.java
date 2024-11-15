07364cad3172d86c8a86d68cd2112311e084a72c
saiprakash-egov <sai.prakash@egovernments.org>
2024-11-15 09:55:26 +0530

    conditionally initialize producer bean

--- health-services/libraries/health-services-common/src/main/java/org/egov/common/producer/Producer.java
+++ health-services/libraries/health-services-common/src/main/java/org/egov/common/producer/Producer.java
@@ -3,12 +3,14 @@ package org.egov.common.producer;
 import lombok.extern.slf4j.Slf4j;
 import org.egov.tracer.kafka.CustomKafkaTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Service;
 
 // NOTE: If tracer is disabled change CustomKafkaTemplate to KafkaTemplate in autowiring
 
 @Service
 @Slf4j
+@ConditionalOnProperty(name = "producer.bean.enabled", havingValue = "true", matchIfMissing = false)
 public class Producer {
 
     private final CustomKafkaTemplate<String, Object> kafkaTemplate;
