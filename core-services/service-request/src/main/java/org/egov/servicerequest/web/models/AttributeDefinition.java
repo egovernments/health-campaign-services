8875f8366d0a7869b23c8023e78b8b1a920f7f6c
Shashwat12-egov <shashwat.k@egovernments.org>
2024-11-22 14:08:59 +0530

    changed the code character size from 64 to 256 (#1147)
    Co-authored-by: shubhang-eGov <70943369+shubhang-eGov@users.noreply.github.com>

--- core-services/service-request/src/main/java/org/egov/servicerequest/web/models/AttributeDefinition.java
+++ core-services/service-request/src/main/java/org/egov/servicerequest/web/models/AttributeDefinition.java
@@ -41,7 +41,7 @@ public class AttributeDefinition {
 
     @JsonProperty("code")
     @NotNull
-    @Size(min = 2, max = 64)
+    @Size(min = 2, max = 256)
     private String code = null;
 
     /**
