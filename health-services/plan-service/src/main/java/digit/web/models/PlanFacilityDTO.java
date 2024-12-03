fbf5ad4ab7cb9c0e22b856a881135a08ec455dad
Tanishi Goyal <tanishi.goyal@egovernments.org>
2024-12-03 16:06:13 +0530

    Adding boundaryAncestralPath for Plan Facility API

--- health-services/plan-service/src/main/java/digit/web/models/PlanFacilityDTO.java
+++ health-services/plan-service/src/main/java/digit/web/models/PlanFacilityDTO.java
@@ -1,6 +1,5 @@
 package digit.web.models;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
