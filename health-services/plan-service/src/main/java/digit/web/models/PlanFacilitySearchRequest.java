ff3f1450b972188a62fa3fdb80abcb4fa4ae29f4
Priyanka-eGov <priyanka@egovernments.org>
2024-12-12 14:19:15 +0530

    HCMPRE-1635 Microplanning v0.1 master merge

---
+++ health-services/plan-service/src/main/java/digit/web/models/PlanFacilitySearchRequest.java
@@ -0,0 +1,34 @@
+package digit.web.models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.egov.common.contract.request.RequestInfo;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.NotNull;
+
+/**
+ * PlanFacilitySearchRequest
+ */
+@Validated
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class PlanFacilitySearchRequest {
+
+    @JsonProperty("RequestInfo")
+    @NotNull
+    @Valid
+    private RequestInfo requestInfo = null;
+
+    @JsonProperty("PlanFacilitySearchCriteria")
+    @NotNull
+    @Valid
+    private PlanFacilitySearchCriteria planFacilitySearchCriteria = null;
+
+}
