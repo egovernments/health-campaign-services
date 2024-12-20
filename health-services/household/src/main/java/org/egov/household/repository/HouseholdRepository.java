6e9f55c1c4c5fde9b3d6db76f6ffb1072f9f2907
shubhang-egov <shubhang.dhanesha@egovernments.org>
2024-12-20 13:39:54 +0530

    fetching null records also

--- health-services/household/src/main/java/org/egov/household/repository/HouseholdRepository.java
+++ health-services/household/src/main/java/org/egov/household/repository/HouseholdRepository.java
@@ -82,8 +82,9 @@ public class HouseholdRepository extends GenericRepository<Household> {
         query = GenericQueryBuilder.generateQuery(query, whereFields).toString();
         query = query.replace("id IN (:id)", "h.id IN (:id)");
         query = query.replace("clientReferenceId IN (:clientReferenceId)", "h.clientReferenceId IN (:clientReferenceId)");
-        if (searchObject.getHouseholdType() != null && searchObject.getHouseholdType().equalsIgnoreCase("FAMILY"))
-            query = query.replace("householdType=:householdType", "householdType!='COMMUNITY'");
+        if (searchObject.getHouseholdType() != null && searchObject.getHouseholdType().equalsIgnoreCase("FAMILY")) {
+            query = query.replace("householdType=:householdType", "householdType!='COMMUNITY' OR householdType IS NULL");
+        }
 
         if(CollectionUtils.isEmpty(whereFields)) {
             query = query + " where h.tenantId=:tenantId ";
@@ -131,6 +132,9 @@ public class HouseholdRepository extends GenericRepository<Household> {
         query = GenericQueryBuilder.generateQuery(query, whereFields).toString();
         query = query.replace("id IN (:id)", "h.id IN (:id)");
         query = query.replace("clientReferenceId IN (:clientReferenceId)", "h.clientReferenceId IN (:clientReferenceId)");
+        if (searchObject.getHouseholdType() != null && searchObject.getHouseholdType().equalsIgnoreCase("FAMILY")) {
+            query = query.replace("householdType=:householdType", "householdType!='COMMUNITY' OR householdType IS NULL");
+        }
 
         if(CollectionUtils.isEmpty(whereFields)) {
             query = query + " where h.tenantId=:tenantId ";
