d8324414b7188a9952f7e2a935d3a3c42bc366fa
ashish-egov <137176738+ashish-egov@users.noreply.github.com>
2025-01-02 12:33:26 +0530

    Allow facility capacity to be 0 in sheet uploads for microplan (#1303)
    * Allow facility capacity to be 0 in sheet uploads for microplan
    
    * changed facility capacity validation message

--- health-services/project-factory/src/server/validators/microplanValidators.ts
+++ health-services/project-factory/src/server/validators/microplanValidators.ts
@@ -269,14 +269,14 @@ function enrichErrorForFcailityMicroplan(request: any, item: any, errors: any = 
         errors.push({ status: "INVALID", rowNumber: item?.["!row#number!"], errorDetails: `Data in ${faciltyStatusColumn} column can’t be empty, please update the data and re-upload` })
     }
     const facilityCapacityColumn = getLocalizedName(`HCM_ADMIN_CONSOLE_FACILITY_CAPACITY_MICROPLAN_${projectType}`, localizationMap);
-    if (!item?.[facilityCapacityColumn]) {
-        errors.push({ status: "INVALID", rowNumber: item?.["!row#number!"], errorDetails: `Data in ${facilityCapacityColumn} column can’t be empty or zero, please update the data and re-upload` })
+    if (!item?.[facilityCapacityColumn] && item?.[facilityCapacityColumn] != 0) {
+        errors.push({ status: "INVALID", rowNumber: item?.["!row#number!"], errorDetails: `Data in ${facilityCapacityColumn} column can’t be empty, please update the data and re-upload` })
     }
     else if (typeof (item?.[facilityCapacityColumn]) != "number") {
-        errors.push({ status: "INVALID", rowNumber: item?.["!row#number!"], errorDetails: `Data in ${facilityCapacityColumn} column must be a number in between 1 and 100000000` })
+        errors.push({ status: "INVALID", rowNumber: item?.["!row#number!"], errorDetails: `Data in ${facilityCapacityColumn} column must be a number from 0 to 100000000` })
     }
-    else if (item?.[facilityCapacityColumn] < 1 || item?.[facilityCapacityColumn] > 100000000) {
-        errors.push({ status: "INVALID", rowNumber: item?.["!row#number!"], errorDetails: `Data in ${facilityCapacityColumn} column must be a number in between 1 and 100000000` })
+    else if (item?.[facilityCapacityColumn] < 0 || item?.[facilityCapacityColumn] > 100000000) {
+        errors.push({ status: "INVALID", rowNumber: item?.["!row#number!"], errorDetails: `Data in ${facilityCapacityColumn} column must be a number from 0 to 100000000` })
     }
     const fixedPostColumn = getLocalizedName("HCM_ADMIN_CONSOLE_FACILITY_FIXED_POST_MICROPLAN", localizationMap);
     if (request?.body?.showFixedPost && !item?.[fixedPostColumn]) {
