de07af90ab3b4e54ef91a87ca1e7830568347f06
yashita-egov <yashita.bansal@egovernments.org>
2025-01-03 12:16:30 +0530

    HCMPRE-874: Model changes (#1305)
    * Beneficiary Type enum added
    
    * clf model changes
    
    * Changed version to 25
    
    ---------
    
    Co-authored-by: shubhang-eGov <70943369+shubhang-eGov@users.noreply.github.com>

--- health-services/libraries/health-services-models/src/main/java/org/egov/common/models/household/Household.java
+++ health-services/libraries/health-services-models/src/main/java/org/egov/common/models/household/Household.java
@@ -27,12 +27,15 @@ public class Household extends EgovOfflineModel {
 
     @JsonProperty("memberCount")
     @NotNull
-    @Range(min = 0, max = 1000)
+//    @Range(min = 0, max = 1000)
     private Integer memberCount = null;
 
     @JsonProperty("address")
     @Valid
     private Address address = null;
+
+    @JsonProperty("householdType")
+    private HouseHoldType householdType = null;
 
     //TODO remove
     @JsonProperty("isDeleted")
