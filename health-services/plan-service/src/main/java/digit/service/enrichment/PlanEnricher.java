f3893e34345e334e65d9bdf9f164ba6e93821671
Tanishi Goyal <tanishi.goyal@egovernments.org>
2025-01-03 14:32:45 +0530

    Adding additional field in plan

--- health-services/plan-service/src/main/java/digit/service/enrichment/PlanEnricher.java
+++ health-services/plan-service/src/main/java/digit/service/enrichment/PlanEnricher.java
@@ -53,6 +53,9 @@ public class PlanEnricher {
         // Generate id for targets
         body.getPlan().getTargets().forEach(target -> UUIDEnrichmentUtil.enrichRandomUuid(target, "id"));
 
+        // Generate id for additional fields
+        body.getPlan().getAdditionalFields().forEach(additionalField -> UUIDEnrichmentUtil.enrichRandomUuid(additionalField, "id"));
+
         // Enrich audit details
         body.getPlan().setAuditDetails(AuditDetailsEnrichmentUtil
                 .prepareAuditDetails(body.getPlan().getAuditDetails(), body.getRequestInfo(), Boolean.TRUE));
@@ -104,6 +107,13 @@ public class PlanEnricher {
         body.getPlan().getTargets().forEach(target -> {
             if(ObjectUtils.isEmpty(target.getId())) {
                 UUIDEnrichmentUtil.enrichRandomUuid(target, "id");
+            }
+        });
+
+        // Generate uuid for new additionalFields
+        body.getPlan().getAdditionalFields().forEach(additionalFields -> {
+            if(ObjectUtils.isEmpty(additionalFields.getId())) {
+                UUIDEnrichmentUtil.enrichRandomUuid(additionalFields, "id");
             }
         });
 
