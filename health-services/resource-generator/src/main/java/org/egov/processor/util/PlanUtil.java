5f523032ceddc00b840780d628bdaa167567ccee
Tanishi Goyal <tanishi.goyal@egovernments.org>
2025-01-20 11:39:34 +0530

    Adding accessibility details in plan additional details

--- health-services/resource-generator/src/main/java/org/egov/processor/util/PlanUtil.java
+++ health-services/resource-generator/src/main/java/org/egov/processor/util/PlanUtil.java
@@ -125,7 +125,7 @@ public class PlanUtil {
 
 			// Creating a map of fields to be added in plan additional details with their key.
 			Map<String, Object> fieldsToBeUpdated = new HashMap<>();
-			if(ObjectUtils.isEmpty(facilityId))
+			if(!ObjectUtils.isEmpty(facilityId))
 				fieldsToBeUpdated.put(FACILITY_ID, facilityId);
 
 			// Add fields from securityDetails to fieldsToBeUpdated map if it's present in censusAdditionalDetails.
