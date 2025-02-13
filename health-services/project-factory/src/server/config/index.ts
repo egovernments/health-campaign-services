12e17a66c608b3ce8284dfdcd578e891821b1bee
ashish-egov <137176738+ashish-egov@users.noreply.github.com>
2025-02-13 11:25:38 +0530

    Update index.ts

--- health-services/project-factory/src/server/config/index.ts
+++ health-services/project-factory/src/server/config/index.ts
@@ -92,7 +92,7 @@ const config = {
     logLevel: process.env.APP_LOG_LEVEL || "debug",
     debugLogCharLimit: process.env.APP_MAX_DEBUG_CHAR ? Number(process.env.APP_MAX_DEBUG_CHAR) : 1000,
     defaultTenantId: process.env.DEFAULT_TENANT_ID || "mz",
-    incomingRequestPayloadLimit : process.env.INCOMING_REQUEST_PAYLOAD_LIMIT || "5mb"
+    incomingRequestPayloadLimit : process.env.INCOMING_REQUEST_PAYLOAD_LIMIT || "2mb"
   },
   localisation: {
     defaultLocale: process.env.LOCALE || "en_MZ",
