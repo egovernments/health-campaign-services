e5fbb1379f70dd6f64c48f93ff52a9f0ceaedb2a
Tanishi Goyal <tanishi.goyal@egovernments.org>
2025-05-06 14:30:50 +0530

    coderabbit review comments

--- health-services/resource-generator/src/main/java/org/egov/processor/web/models/DraftDetails.java
+++ health-services/resource-generator/src/main/java/org/egov/processor/web/models/DraftDetails.java
@@ -1,12 +1,17 @@
 package org.egov.processor.web.models;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class DraftDetails {
     @JsonProperty("planConfigurationId")
     @NotNull
