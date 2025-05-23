210e39086fa9d8ee511d555d7725d7f1d13079c2
arnob-equidhi <arnob@beehyv.com>
2025-05-23 17:59:36 +0530

    revrt commons lib changes

--- health-services/libraries/health-services-common/src/main/java/org/egov/common/utils/ValidatorUtils.java
+++ health-services/libraries/health-services-common/src/main/java/org/egov/common/utils/ValidatorUtils.java
@@ -4,7 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 
 import org.egov.common.exception.InvalidTenantIdException;
 import org.egov.common.models.Error;
-import org.egov.common.models.Error.ErrorType;
 import org.egov.tracer.model.CustomException;
 
 import java.util.List;
@@ -114,13 +113,5 @@ public class ValidatorUtils {
                 .exception(new CustomException("IS_DELETED_TRUE_SUB_ENTITY", "isDeleted cannot be true for sub entity"))
                 .build();
     }
-
-   public static Error getErrorForInvalidTenantId(String tenantId, InvalidTenantIdException exception) {
-      return Error.builder()
-                .errorMessage(String.format("tenantId : %s is not valid", tenantId))
-                .errorCode("TENANT_ID_INVALID")
-                .type(ErrorType.NON_RECOVERABLE).exception(exception)
-                .build();
-   }
 
 }
