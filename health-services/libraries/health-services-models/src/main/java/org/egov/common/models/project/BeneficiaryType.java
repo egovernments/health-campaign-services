64b4eecbd6f3bd99897e8eee29bbb957aa6d81fc
arnob-equidhi <arnob@beehyv.com>
2025-05-23 15:23:39 +0530

    add Beneficiary Types required for Bauchi SMC+VAS campaign

--- health-services/libraries/health-services-models/src/main/java/org/egov/common/models/project/BeneficiaryType.java
+++ health-services/libraries/health-services-models/src/main/java/org/egov/common/models/project/BeneficiaryType.java
@@ -3,37 +3,52 @@ package org.egov.common.models.project;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
 
-
 public enum BeneficiaryType {
 
-		HOUSEHOLD("HOUSEHOLD"),
+	HOUSEHOLD("HOUSEHOLD"),
 
-		INDIVIDUAL("INDIVIDUAL"),
+	INDIVIDUAL("INDIVIDUAL"),
 
-		PRODUCT("PRODUCT"),
+	PRODUCT("PRODUCT"),
 
-		SCHOOL("SCHOOL");
+	SCHOOL("SCHOOL")
 
-		private String value;
+	SPAQ_1("SPAQ-1"),
 
-		BeneficiaryType(String value) {
-			this.value = value;
-		}
+	SPAQ_2("SPAQ-2"),
 
-		@Override
-		@JsonValue
-		public String toString() {
-			return String.valueOf(value);
-		}
+	THREE_ELEVEN_MONTH("3-11MONTH"),
 
-		@JsonCreator
-		public static BeneficiaryType fromValue(String text) {
-			for(BeneficiaryType a:BeneficiaryType.values()){
-				if(String.valueOf(a.value).equalsIgnoreCase(text)){
-					return a;
-				}
+	TWELVE_FIFTY_NINE_MONTH("12-59MONTH"),
+
+	SIX_ELEVEN_MONTH("6-11MONTH"),
+
+	PRODUCT("PRODUCT"),
+
+	VAS_RED("VAS-RED"),
+
+	VAS_BLUE("VAS-BLUE");
+
+	private String value;
+
+	BeneficiaryType(String value) {
+		this.value = value;
+	}
+
+	@Override
+	@JsonValue
+	public String toString() {
+		return String.valueOf(value);
+	}
+
+	@JsonCreator
+	public static BeneficiaryType fromValue(String text) {
+		for(BeneficiaryType a:BeneficiaryType.values()){
+			if(String.valueOf(a.value).equalsIgnoreCase(text)){
+				return a;
 			}
-
-			return null;
 		}
+
+		return null;
+	}
 }
