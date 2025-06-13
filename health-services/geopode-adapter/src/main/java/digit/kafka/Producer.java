9f57dccf4773e6aa1445ddfab55b6433e6bcf4de
Abishek <abishek.t@egovernments.org>
2025-06-13 10:32:20 +0530

    Adding @Autowired

--- health-services/geopode-adapter/src/main/java/digit/kafka/Producer.java
+++ health-services/geopode-adapter/src/main/java/digit/kafka/Producer.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Service;
 @Service
 @Slf4j
 public class Producer {
-
+    @Autowired
     private CustomKafkaTemplate<String, Object> kafkaTemplate;
 
     public void push(String topic, Object value) {
