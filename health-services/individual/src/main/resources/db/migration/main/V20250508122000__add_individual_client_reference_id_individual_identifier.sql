048bdf3014a59c0faef50b26ea2755de38ff4453
Holash Chand <holash.c@beehyv.com>
2025-06-18 13:17:09 +0530

    Modified migration script for individual service

--- health-services/individual/src/main/resources/db/migration/main/V20250508122000__add_individual_client_reference_id_individual_identifier.sql
+++ health-services/individual/src/main/resources/db/migration/main/V20250508122000__add_individual_client_reference_id_individual_identifier.sql
@@ -1,8 +1,19 @@
 -- Rename the existing column clientReferenceId
--- Since it's being used as individual client reference id
+-- 1. Since it's being used as individual client reference id
 ALTER TABLE INDIVIDUAL_IDENTIFIER
     RENAME COLUMN clientReferenceId TO individualClientReferenceId;
 
--- Add a new clientReferenceId column
+-- 2. Drop the unique constraint (if any)
 ALTER TABLE INDIVIDUAL_IDENTIFIER
-    ADD COLUMN IF NOT EXISTS clientReferenceId character varying(64);
+    DROP CONSTRAINT IF EXISTS individual_identifier_clientreferenceid_key;
+
+-- 3. Add a new clientReferenceId column
+ALTER TABLE INDIVIDUAL_IDENTIFIER
+    ADD COLUMN IF NOT EXISTS clientReferenceId character varying(64)
+    DEFAULT gen_random_uuid()::text UNIQUE;
+
+-- 4. Fill existing clientReferenceId column with null uuid values
+UPDATE INDIVIDUAL_IDENTIFIER
+    SET clientReferenceId = gen_random_uuid()::text
+    WHERE clientReferenceId IS NULL;
+
