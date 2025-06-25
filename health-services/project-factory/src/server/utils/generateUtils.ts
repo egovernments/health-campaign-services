86a2cf6713cb115d07b2d73a422bd759f2255414
nitish-egov <137176807+nitish-egov@users.noreply.github.com>
2025-06-25 16:51:33 +0530

    removed throwerror and added log.error instead

--- health-services/project-factory/src/server/utils/generateUtils.ts
+++ health-services/project-factory/src/server/utils/generateUtils.ts
@@ -1,4 +1,4 @@
-import { getLocalizedMessagesHandler, processGenerate, replicateRequest, throwError } from "./genericUtils";
+import { getLocalizedMessagesHandler, processGenerate, replicateRequest } from "./genericUtils";
 import _ from 'lodash';
 import { getFormattedStringForDebug, logger } from "./logger";
 import { getBoundarySheetData } from "../api/genericApis";
@@ -67,7 +67,7 @@ async function callGenerateIfBoundariesOrCampaignTypeDiffer(request: any) {
         }
     } catch (error: any) {
         logger.error(error);
-        throwError("COMMON", 400, "GENERATE_ERROR", `Error while generating user/facility/boundary: ${error.message}`);
+        // throwError("COMMON", 400, "GENERATE_ERROR", `Error while generating user/facility/boundary: ${error.message}`);
     }
 }
 
