7dab51e8ae3ea6edbf4715cc7e668f67620032ae
ashish-egov <ashish.tiwari@egovernments.org>
2025-08-08 10:04:38 +0530

    Revert "Ingestion initialised"
    This reverts commit 16709f3ebb1b8325d3b40a340d79b78e2b006490.

--- health-services/excel-ingestion/src/main/java/org/egov/excelingestion/web/processor/IGenerateProcessor.java
+++
@@ -1,9 +0,0 @@
-package org.egov.excelingestion.web.processor;
-
-import org.egov.excelingestion.web.models.GeneratedResource;
-import org.egov.excelingestion.web.models.GeneratedResourceRequest;
-
-public interface IGenerateProcessor {
-    GeneratedResource process(GeneratedResourceRequest request);
-    String getType();
-}
