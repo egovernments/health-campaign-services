30744fc696123f0b10ea4eefe20dcfe109723d7c
Ritik Verma <ritik.kumar@egovernments.org>
2026-01-14 17:28:12 +0530

    config fix (#1854)

--- health-services/project-factory/src/server/config/index.ts
+++ health-services/project-factory/src/server/config/index.ts
@@ -196,7 +196,10 @@ const config = {
     maxHttpRetries: process.env.MAX_HTTP_RETRIES || "4",
     autoRetryIfHttpError: process.env.AUTO_RETRY_IF_HTTP_ERROR || "socket hang up" /* can be retry if there is any error for which default retry can be set */,
     latLongColumns: process.env.LAT_LONG_SUBSTRINGS || "HCM_ADMIN_CONSOLE_FACILITY_LATITUDE_OPTIONAL_MICROPLAN,HCM_ADMIN_CONSOLE_FACILITY_LONGITUDE_OPTIONAL_MICROPLAN,HCM_ADMIN_CONSOLE_TARGET_LAT_OPT,HCM_ADMIN_CONSOLE_TARGET_LONG_OPT",
-    validateCampaignIdInMetadata: process.env.VALIDATE_CAMPAIGN_ID_IN_METADATA === "true"
+    validateCampaignIdInMetadata: process.env.VALIDATE_CAMPAIGN_ID_IN_METADATA === "true",
+    IS_BEDNET_CAMPAIGN_TYPE_TRUE: "campaign-create-bednet",
+    IS_BEDNET_CAMPAIGN_TYPE_FALSE: "campaign-create"
+
   },
   resourceCreationConfig: {
     maxAttemptsForResourceCreationOrMapping: Number(process.env.MAX_RESOURCE_CREATION_ATTEMPTS || 200),
