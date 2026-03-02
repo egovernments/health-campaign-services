cf939c2d1e29af76105f748fc700a309d97b9361
ritikegov <ritik.kumar@egovernments.org>
2026-03-02 14:19:10 +0530

    change the constant file

--- health-services/health-notification-service/src/main/java/org/egov/healthnotification/Constants.java
+++ health-services/health-notification-service/src/main/java/org/egov/healthnotification/Constants.java
@@ -1,9 +1,17 @@
 package org.egov.healthnotification;
 
-public interface Constants {
-    String VALIDATION_ERROR = "VALIDATION_ERROR";
-    String INVALID_TENANT_ID = "INVALID_TENANT_ID";
-    String INVALID_TENANT_ID_MSG = "Tenant ID is not valid";
-    String DELIVERY_STRATEGY_KEY = "deliveryStrategy";
-    String DELIVERY_STRATEGY_DIRECT = "DIRECT";
+public class Constants {
+
+    private Constants() {
+        throw new IllegalStateException("Constants class");
+    }
+
+    // Validation error constants
+    public static final String VALIDATION_ERROR = "VALIDATION_ERROR";
+    public static final String INVALID_TENANT_ID = "INVALID_TENANT_ID";
+    public static final String INVALID_TENANT_ID_MSG = "Tenant ID is not valid";
+
+    // Task filtering constants
+    public static final String DELIVERY_STRATEGY_KEY = "deliveryStrategy";
+    public static final String DELIVERY_STRATEGY_DIRECT = "DIRECT";
 }
