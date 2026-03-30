0151860c166a0e27946df48052c12a1fb2a56d65
hruthvikl-egov <hruthvik.lingampet@egovernments.org>
2026-03-30 15:14:54 +0530

    Final Message topic step addition-IN_PROGRESS and indexer fix.

--- health-services/health-notification-service/src/main/java/org/egov/healthnotification/web/models/enums/NotificationStatus.java
+++ health-services/health-notification-service/src/main/java/org/egov/healthnotification/web/models/enums/NotificationStatus.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonValue;
 
 public enum NotificationStatus {
     PENDING("PENDING"),
+    IN_PROGRESS("IN_PROGRESS"),
     SENT("SENT"),
     FAILED("FAILED"),
     CANCELLED("CANCELLED");
