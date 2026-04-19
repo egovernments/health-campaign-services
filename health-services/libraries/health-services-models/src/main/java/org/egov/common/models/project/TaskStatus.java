9445b51ab6586419bd4552993866fa289f5c85d8
bhlp0151 <sarasani.vishalreddy@equidhi.org>
2026-04-19 11:58:16 +0530

    added new task status and removed rowversion validaiton for user action

--- health-services/libraries/health-services-models/src/main/java/org/egov/common/models/project/TaskStatus.java
+++ health-services/libraries/health-services-models/src/main/java/org/egov/common/models/project/TaskStatus.java
@@ -84,7 +84,12 @@ public enum TaskStatus {
      * This status signifies that the task could not be completed because
      * the beneficiary was not present at the time of the visit.
      */
-    BENEFICIARY_ABSENT("BENEFICIARY_ABSENT");
+    BENEFICIARY_ABSENT("BENEFICIARY_ABSENT"),
+
+    /**
+     * Indicates that the beneficiary was given redose.
+     */
+    VISITED("VISITED");
 
     // The string value associated with the task status.
     private String value;
