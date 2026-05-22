baa0dbd8f1fff18f34d5c261c42fcca9338aae4a
LataNaik-eGov <lata.naik@moolya.com>
2026-05-22 16:17:21 +0530

    Added annotation for generate and search request and response

--- health-services/referralmanagement/src/main/java/org/egov/referralmanagement/web/models/DownsyncJobSearchRequest.java
+++ health-services/referralmanagement/src/main/java/org/egov/referralmanagement/web/models/DownsyncJobSearchRequest.java
@@ -1,5 +1,6 @@
 package org.egov.referralmanagement.web.models;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -12,6 +13,7 @@ import org.egov.common.contract.request.RequestInfo;
 @NoArgsConstructor
 @AllArgsConstructor
 public class DownsyncJobSearchRequest {
+    @JsonProperty("RequestInfo")
     private RequestInfo requestInfo;
 
     @NotBlank(message = "jobId is required")
