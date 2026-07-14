15934cc33bf49d055fb70b93eeaddce5226afc7e
hruthvikl-egov <hruthvik.lingampet@egovernments.org>
2026-07-14 17:38:19 +0530

    ES_REFERRAL_INDEX update to demo URL

--- utilities/hcm-custom-reports/REPORTS_GENERATION/REPORTS/stock_report/stock_report.py
+++ utilities/hcm-custom-reports/REPORTS_GENERATION/REPORTS/stock_report/stock_report.py
@@ -36,8 +36,8 @@ from REPORTS_GENERATION.COMMON_UTILS.custom_date_utils import get_custom_dates_o
 # ===========================
 # CONFIG
 # ===========================
-ES_STOCK_SEARCH = "http://elasticsearch-master.es-upgrade.svc.cluster.local:9200/stock-index-v1/_search"
-ES_SCROLL_API = "http://elasticsearch-master.es-upgrade.svc.cluster.local:9200/_search/scroll"
+ES_STOCK_SEARCH = "http://elasticsearch-master.es-cluster.svc.cluster.local:9200/stock-index-v1/_search"
+ES_SCROLL_API = "http://elasticsearch-master.es-cluster.svc.cluster.local:9200/_search/scroll"
 
 ELASTIC_AUTH = "Basic ZWxhc3RpYzo4ZndiRDZIYkpoNkhVMG9kZHNIbThURUk="
 
